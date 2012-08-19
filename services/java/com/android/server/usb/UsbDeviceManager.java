d726dad2512b66d9932df777d89c32d52319d653
Rashed Abdel-Tawab <rashedabdeltawab@gmail.com>
2012-08-19 06:36:43 -0400

    Rework usb mount
    * Changed the red "Beandroid" on the USB connected page to
    a white USB icon.
    
    * Cleaned comments
    
    Change-Id: Iee4c58769369e6dbe8d131eb4a2d9bee49d1e99b

--- services/java/com/android/server/usb/UsbDeviceManager.java
+++ services/java/com/android/server/usb/UsbDeviceManager.java
@@ -643,10 +643,7 @@ public class UsbDeviceManager {
                     id = com.android.internal.R.string.usb_mtp_notification_title;
                 } else if (containsFunction(mCurrentFunctions, UsbManager.USB_FUNCTION_PTP)) {
                     id = com.android.internal.R.string.usb_ptp_notification_title;
-                } /* else if (containsFunction(mCurrentFunctions,
-                     UsbManager.USB_FUNCTION_MASS_STORAGE)) { // Disable this as it causes double USB settings menues when in UMS mode.
-                     id = com.android.internal.R.string.usb_cd_installer_notification_title; 
-                     } */ else if (containsFunction(mCurrentFunctions, UsbManager.USB_FUNCTION_ACCESSORY)) {
+                } else if (containsFunction(mCurrentFunctions, UsbManager.USB_FUNCTION_ACCESSORY)) {
                     id = com.android.internal.R.string.usb_accessory_notification_title;
                 } else {
                     // There is a different notification for USB tethering so we don't need one here
