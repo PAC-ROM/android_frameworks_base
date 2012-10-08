5c5139a3a706db599014b4533b20895bcc58e4eb
PaulHenschel <drcmda@gmail.com>
2012-10-08 10:08:29 +0200

    typo
    Change-Id: I6174d052a28caf698371223a8a8011ada86509ff

--- core/java/android/util/ExtendedPropertiesUtils.java
+++ core/java/android/util/ExtendedPropertiesUtils.java
@@ -42,7 +42,7 @@ import java.util.List;
 
 public class ExtendedPropertiesUtils {
  
-    private static final String TAG = "ExtendedPropertiesUtils";
+    private static final String TAG = "paranoid";
 
     /**
      * Public variables
