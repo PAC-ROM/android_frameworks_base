05374dd4af6ded11a3f53ff36d6cb7ca1048ef1a
DvTonder <david.vantonder@gmail.com>
2012-11-24 10:43:30 -0500

    Framework: Forward port Power menu config
    Change-Id: I4836ddf43e82e254aefa3933a1e30fd652f08f90
    
    Conflicts:
    
    	policy/src/com/android/internal/policy/impl/GlobalActions.java

--- policy/src/com/android/internal/policy/impl/GlobalActions.java
+++ policy/src/com/android/internal/policy/impl/GlobalActions.java
@@ -128,10 +128,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
     private boolean mHasTelephony;
     private boolean mHasVibrator;
     private boolean mRebootMenu;
-
     private Profile mChosenProfile;
-
-    private static final String SYSTEM_PROFILES_ENABLED = "system_profiles_enabled";
 
     /**
      * @param context everything needs a context :(
@@ -328,31 +325,39 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
             );
 
             // next: reboot
-            mItems.add(
-                new SinglePressAction(R.drawable.ic_lock_reboot, R.string.global_action_reboot) {
-                    public void onPress() {
-                        showDialog(mKeyguardShowing, mDeviceProvisioned, true);
-                    }
+            // only shown if enabled, enabled by default
+            if (Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.POWER_MENU_REBOOT_ENABLED, 1) == 1) {
+                mItems.add(
+                    new SinglePressAction(R.drawable.ic_lock_reboot, R.string.global_action_reboot) {
+                        public void onPress() {
+                            showDialog(mKeyguardShowing, mDeviceProvisioned, true);
+                        }
 
-                    public boolean onLongPress() {
-                        return true;
-                    }
+                        public boolean onLongPress() {
+                            return true;
+                        }
 
-                    public boolean showDuringKeyguard() {
-                        return true;
-                    }
+                        public boolean showDuringKeyguard() {
+                            return true;
+                        }
 
-                    public boolean showBeforeProvisioning() {
-                        return true;
+                        public boolean showBeforeProvisioning() {
+                            return true;
+                        }
                     }
-                }
-            );
+                );
+            }
 
             // next: expanded desktop
             mItems.add(mExpandDesktopModeOn);
 
-            // next: profile - only shown if enabled, which is true by default
-            if (Settings.System.getInt(mContext.getContentResolver(), SYSTEM_PROFILES_ENABLED, 1) == 1) {
+            // next: profile
+            // only shown if both system profiles and the menu item is enabled, enabled by default
+            if ((Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.SYSTEM_PROFILES_ENABLED, 1) == 1) &&
+                    (Settings.System.getInt(mContext.getContentResolver(),
+                            Settings.System.POWER_MENU_PROFILES_ENABLED, 1) == 1)) {                mItems.add(
                 mItems.add(
                     new ProfileChooseAction() {
                         public void onPress() {
@@ -375,24 +380,31 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
             }
 
             // next: screenshot
-            mItems.add(
-                new SinglePressAction(R.drawable.ic_lock_screenshot, R.string.global_action_screenshot) {
-                    public void onPress() {
-                        takeScreenshot();
-                    }
+            // only shown if enabled, disabled by default
+            if (Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.POWER_MENU_SCREENSHOT_ENABLED, 0) == 1) {
+                mItems.add(
+                    new SinglePressAction(R.drawable.ic_lock_screenshot, R.string.global_action_screenshot) {
+                        public void onPress() {
+                            takeScreenshot();
+                        }
 
-                    public boolean showDuringKeyguard() {
-                        return true;
-                    }
+                        public boolean showDuringKeyguard() {
+                            return true;
+                        }
 
-                    public boolean showBeforeProvisioning() {
-                        return true;
+                        public boolean showBeforeProvisioning() {
+                            return true;
+                        }
                     }
-                }
-            );
+                );
+            }
 
             // next: airplane mode
-            mItems.add(mAirplaneModeOn);
+            if (Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.POWER_MENU_AIRPLANE_ENABLED, 1) == 1) {
+                mItems.add(mAirplaneModeOn);
+            }
 
             // next: bug report, if enabled
             if (Settings.Secure.getInt(mContext.getContentResolver(),
@@ -443,14 +455,16 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
                     });
             }
 
-            // last: silent mode
-            if (SHOW_SILENT_TOGGLE) {
-                mItems.add(mSilentModeAction);
-            }
-
-            // one more thing: optionally add a list of users to switch to
+            // next: optionally add a list of users to switch to
             if (SystemProperties.getBoolean("fw.power_user_switcher", false)) {
                 addUsersToMenu(mItems);
+            }
+
+            // last: silent mode
+            if ((Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.POWER_MENU_SILENT_ENABLED, 1) == 1) &&
+                    (SHOW_SILENT_TOGGLE)) {
+                mItems.add(mSilentModeAction);
             }
         }
 
@@ -474,6 +488,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
                         return mAdapter.getItem(position).onLongPress();
                     }
         });
+
         dialog.getWindow().setType(WindowManager.LayoutParams.TYPE_KEYGUARD_DIALOG);
         dialog.setOnDismissListener(this);
 
