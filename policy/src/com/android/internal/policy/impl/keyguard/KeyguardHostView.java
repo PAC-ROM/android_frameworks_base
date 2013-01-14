ea3f05d36c7a369fe5545e4e46966f391e3d2232
Dirk Rettschlag <dirk.rettschlag@gmail.com>
2013-01-14 21:42:16 +0100

    Revert "Lockscreen: longpress on expand challenge handle"
    (reverse-merged from commit 25e216b4fa5028e013abaff0daed7805c8c27f3a)
    
    Change-Id: I5b7f401ba34c3ef28dc20c7f760fffb7bcfebb6a

--- policy/src/com/android/internal/policy/impl/keyguard/KeyguardHostView.java
+++ policy/src/com/android/internal/policy/impl/keyguard/KeyguardHostView.java
@@ -52,7 +52,6 @@ import android.provider.Settings;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.util.Slog;
-import android.view.HapticFeedbackConstants;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
@@ -87,7 +86,6 @@ public class KeyguardHostView extends KeyguardViewBase {
     private KeyguardSecurityViewFlipper mSecurityViewContainer;
     private KeyguardSelectorView mKeyguardSelectorView;
     private KeyguardTransportControlView mTransportControl;
-    private View mExpandChallengeView;
     private boolean mIsVerifyUnlockOnly;
     private boolean mEnableFallback; // TODO: This should get the value from KeyguardPatternView
     private SecurityMode mCurrentSecuritySelection = SecurityMode.Invalid;
@@ -270,26 +268,8 @@ public class KeyguardHostView extends KeyguardViewBase {
 
         showPrimarySecurityScreen(false);
         updateSecurityViews();
-
-        mExpandChallengeView = (View) findViewById(R.id.expand_challenge_handle);
-        if (mExpandChallengeView != null) {
-            mExpandChallengeView.setOnLongClickListener(mFastUnlockClickListener);
-        }
-
         minimizeChallengeIfNeeded();
     }
-
-    private final OnLongClickListener mFastUnlockClickListener = new OnLongClickListener() {
-        @Override
-        public boolean onLongClick(View v) {
-            if (mLockPatternUtils.isTactileFeedbackEnabled()) {
-                v.performHapticFeedback(HapticFeedbackConstants.LONG_PRESS,
-                        HapticFeedbackConstants.FLAG_IGNORE_GLOBAL_SETTING);
-            }
-            showNextSecurityScreenOrFinish(false);
-            return true;
-        }
-    };
 
     private void updateBackground() {
         String background = Settings.System.getStringForUser(getContext().getContentResolver(),
