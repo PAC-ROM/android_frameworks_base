a20d9418d66cc8607ba6645ca76b896433cf9c12
Gergely Szell (sethyx) <szell@hu.ibm.com>
2013-01-25 22:26:46 +0100

    RecentsBoost++
    Change-Id: I4dbaaa31d620ced287df12a0f861467ed2bd8a5b
    Signed-off-by: Gergely Szell (sethyx) <szell@hu.ibm.com>

--- packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
+++ packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
@@ -116,6 +116,7 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
     Handler mHandler = new Handler();
     SettingsObserver mSettingsObserver;
     ActivityManager mAm;
+    ActivityManager.MemoryInfo mMemInfo;
 
     MemInfoReader mMemInfoReader = new MemInfoReader();
 
@@ -289,6 +290,7 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
 
         mAm = (ActivityManager)
                 mContext.getSystemService(Context.ACTIVITY_SERVICE);
+        mMemInfo = new ActivityManager.MemoryInfo();
         TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.RecentsPanelView,
                 defStyle, 0);
 
@@ -854,9 +856,8 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
             if (!ramBarEnabled)
                 return;
 
-            ActivityManager.MemoryInfo memInfo = new ActivityManager.MemoryInfo();
-            mAm.getMemoryInfo(memInfo);
-            long secServerMem = memInfo.secondaryServerThreshold;
+            mAm.getMemoryInfo(mMemInfo);
+            long secServerMem = mMemInfo.secondaryServerThreshold;
             mMemInfoReader.readMemInfo();
             long availMem = mMemInfoReader.getFreeSize() + mMemInfoReader.getCachedSize() -
                     secServerMem;
