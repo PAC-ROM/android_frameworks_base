439a2cf475714fa754baaacd71b30a42feeb4d09
szl.kiev <szl.kiev@gmail.com>
2013-02-16 11:42:53 +0700

    Revert "Revert "Forward port Volume panel style and Linked volumes""
    (reverse-merged from commit 162376e7b48368bc480b1363376474dc1d0d8fba)
    
    Change-Id: I652cbf902defb13f0433c6cbb73df5ad2631f57a

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -1910,6 +1910,22 @@ public final class Settings {
         public static final String ALWAYS_FINISH_ACTIVITIES = Global.ALWAYS_FINISH_ACTIVITIES;
 
         /**
+         * Volume Overlay Mode, This is behaviour of the volume overlay panel
+         * Defaults to 0 - which is simple
+         * @hide
+         */
+        public static final String MODE_VOLUME_OVERLAY = "mode_volume_overlay";
+
+        /** @hide */
+        public static final int VOLUME_OVERLAY_SINGLE = 0;
+        /** @hide */
+        public static final int VOLUME_OVERLAY_EXPANDABLE = 1;
+        /** @hide */
+        public static final int VOLUME_OVERLAY_EXPANDED = 2;
+        /** @hide */
+        public static final int VOLUME_OVERLAY_NONE = 3;
+
+        /**
          * Determines which streams are affected by ringer mode changes. The
          * stream type's bit should be set to 1 if it should be muted when going
          * into an inaudible ringer mode.
@@ -2759,6 +2775,13 @@ public final class Settings {
          * @hide
          */
         public static final String SCREEN_LOCK_SLIDE_SCREENOFF_DELAY = "screen_lock_slide_screenoff_delay";
+
+        /**
+         * Whether to link ringtone and notification volumes
+         * The value is boolean (1 or 0).
+         * @hide
+         */
+        public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
 
         /**
          * Whether to unlock the menu key.  The value is boolean (1 or 0).
