d96665ad4baec458e51899129050040f26ca567f
blindndumb <blindndumb@gmail.com>
2013-03-12 11:36:15 -0700

    PIE: Restart launcher

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2669,6 +2669,12 @@ public final class Settings {
          * @hide
          */
         public static final String EXPANDED_DESKTOP_STATE = "expanded_desktop_state";
+		
+		/**
+		* Restart Launcher
+		* @hide
+		*/
+        public static final String EXPANDED_DESKTOP_RESTART_LAUNCHER = "expanded_desktop_restart_launcher";
 
         /**
          * Expanded desktop style (with status bar or without status bar)
