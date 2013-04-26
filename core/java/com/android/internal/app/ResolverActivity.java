47f6ce4b2f4dca0335141e5e3a6f61e335f05a4a
Sniper- <markp.mckinney@gmail.com>
2013-04-25 23:01:40 -0700

    Whoops.... Fix build again

--- core/java/com/android/internal/app/ResolverActivity.java
+++ core/java/com/android/internal/app/ResolverActivity.java
@@ -44,11 +44,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
-import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -73,16 +72,14 @@ public class ResolverActivity extends AlertActivity implements AdapterView.OnIte
     private boolean mAlwaysUseOption;
     private boolean mShowExtended;
     private GridView mGrid;
-    private Button mAlwaysButton;
-    private Button mOnceButton;
-    private CheckBox mAlwaysCheckBox;
+    private RadioButton mAlwaysButton;
+    private RadioButton mOnceButton;
     private int mIconDpi;
     private int mIconSize;
     private int mMaxColumns;
     private int mLastSelected = GridView.INVALID_POSITION;
 
     private boolean mRegistered;
-    private int mUseAltGrid;
     private final PackageMonitor mPackageMonitor = new PackageMonitor() {
         @Override public void onSomePackagesChanged() {
             mAdapter.handlePackagesChanged();
@@ -124,7 +121,6 @@ public class ResolverActivity extends AlertActivity implements AdapterView.OnIte
             mLaunchedFromUid = -1;
         }
         mPm = getPackageManager();
-        mUseAltGrid = Settings.System.getInt(getContentResolver(), Settings.System.ACTIVITY_RESOLVER_USE_ALT, 0);
         mAlwaysUseOption = alwaysUseOption;
         mMaxColumns = getResources().getInteger(R.integer.config_maxResolverActivityColumns);
         intent.setComponent(null);
@@ -148,11 +144,7 @@ public class ResolverActivity extends AlertActivity implements AdapterView.OnIte
             finish();
             return;
         } else if (count > 1) {
-            if (mUseAltGrid == 1) {
-                ap.mView = getLayoutInflater().inflate(R.layout.resolver_grid_alt, null);
-            } else {
-                ap.mView = getLayoutInflater().inflate(R.layout.resolver_grid, null);
-            }
+            ap.mView = getLayoutInflater().inflate(R.layout.resolver_grid, null);
             mGrid = (GridView) ap.mView.findViewById(R.id.resolver_grid);
             mGrid.setAdapter(mAdapter);
             mGrid.setOnItemClickListener(this);
@@ -179,12 +171,8 @@ public class ResolverActivity extends AlertActivity implements AdapterView.OnIte
             final ViewGroup buttonLayout = (ViewGroup) findViewById(R.id.button_bar);
             if (buttonLayout != null) {
                 buttonLayout.setVisibility(View.VISIBLE);
-                if (mUseAltGrid == 1) {
-                    mAlwaysCheckBox = (CheckBox) buttonLayout.findViewById(R.id.checkbox_always);
-                } else {
-                    mAlwaysButton = (Button) buttonLayout.findViewById(R.id.button_always);
-                    mOnceButton = (Button) buttonLayout.findViewById(R.id.button_once);
-                }
+                mAlwaysButton = (RadioButton) buttonLayout.findViewById(R.id.button_always);
+                mOnceButton = (RadioButton) buttonLayout.findViewById(R.id.button_once);
             } else {
                 mAlwaysUseOption = false;
             }
@@ -267,10 +255,6 @@ public class ResolverActivity extends AlertActivity implements AdapterView.OnIte
             final int checkedPos = mGrid.getCheckedItemPosition();
             final boolean enabled = checkedPos != GridView.INVALID_POSITION;
             mLastSelected = checkedPos;
-            if (mUseAltGrid == 0) {
-                mAlwaysButton.setEnabled(enabled);
-                mOnceButton.setEnabled(enabled);
-            }
             if (enabled) {
                 mGrid.setSelection(checkedPos);
             }
@@ -282,27 +266,13 @@ public class ResolverActivity extends AlertActivity implements AdapterView.OnIte
         final int checkedPos = mGrid.getCheckedItemPosition();
         final boolean hasValidSelection = checkedPos != GridView.INVALID_POSITION;
         if (mAlwaysUseOption && (!hasValidSelection || mLastSelected != checkedPos)) {
-            if (mUseAltGrid == 0) {
-                mAlwaysButton.setEnabled(hasValidSelection);
-                mOnceButton.setEnabled(hasValidSelection);
-            }
-            if (hasValidSelection) {
-                if (mUseAltGrid == 1) {
-                    startSelected(position,mAlwaysCheckBox.isChecked());
-                } else {
-                    mGrid.smoothScrollToPosition(checkedPos);
-                }
+           if (hasValidSelection) {
+               startSelected(position, mAlwaysButton.isChecked());
             }
             mLastSelected = checkedPos;
         } else {
             startSelected(position, false);
         }
-    }
-
-    public void onButtonClick(View v) {
-        final int id = v.getId();
-        startSelected(mGrid.getCheckedItemPosition(), id == R.id.button_always);
-        dismiss();
     }
 
     void startSelected(int which, boolean always) {
