b4bdc1f7f6efe1822363a49b765a19540b77153c
Lars Greiss <kufikugel@googlemail.com>
2013-06-06 01:29:12 +0200

    Configurable Notification Behavior [2/2]
    Change-Id: Ibb6bd69d4df0bdd664a88213aaab0385ac33a139

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4618,6 +4618,12 @@ public final class Settings {
         public static final String GESTURE_BLACKLIST = "gesture_blacklist";
 
         /**
+         * Notifications Expand Behavior
+         * @hide
+         */
+        public static final String NOTIFICATIONS_BEHAVIOUR = "notifications_behaviour"; 
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
