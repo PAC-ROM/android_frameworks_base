22cb08ccc9a5c9f1784adce2288d1760cff39d98
Danny Baumann <dannybaumann@web.de>
2013-07-01 11:53:49 +0200

    Add an option to allow a (subtle) notification vibration during calls.
    Change-Id: I76c6beda31859c5531046d1567f06b095a8ca4c4

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4307,6 +4307,14 @@ public final class Settings {
         public static final String LOCKSCREEN_LONGPRESS_CHALLENGE = "lockscreen_longpress_challenge";
 
         /**
+         * Whether to allow notification vibration while notification alerts are disabled
+         * (e.g. during phone calls). The vibration pattern to be used will be a subtle one;
+         * custom vibration is disabled at that point.
+         * @hide
+         */
+        public static final String NOTIFICATION_VIBRATE_DURING_ALERTS_DISABLED = "vibrate_while_no_alerts";
+
+        /**
          * enable and disable fast toggle in settings
          *
          * @hide
@@ -4399,6 +4407,7 @@ public final class Settings {
             LOCKSCREEN_SOUNDS_ENABLED,
             SHOW_WEB_SUGGESTIONS,
             NOTIFICATION_LIGHT_PULSE,
+            NOTIFICATION_VIBRATE_DURING_ALERTS_DISABLED,
             SIP_CALL_OPTIONS,
             SIP_RECEIVE_CALLS,
             POINTER_SPEED,
