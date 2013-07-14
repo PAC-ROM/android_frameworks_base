e59f25ae9b70cd3f5e25660c702eee62bd982e44
Danny Baumann <dannybaumann@web.de>
2013-07-14 13:12:50 +0200

    Improve notification shade collapse code.
    - Collapse after dismissing the last notification
    - Avoid duplicate code paths for collapsing
    - Improve variable naming according to AOSP review suggestions
    
    Change-Id: Ic6f26a61f263c5beebbcc9725fe8914d3858576f

--- packages/SystemUI/src/com/android/systemui/statusbar/tv/TvStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/tv/TvStatusBar.java
@@ -151,6 +151,11 @@ public class TvStatusBar extends BaseStatusBar {
     }
 
     @Override
+    protected boolean isNotificationPanelFullyVisible() {
+        return false;
+    }
+
+    @Override
     protected boolean shouldDisableNavbarGestures() {
         return true;
     }
