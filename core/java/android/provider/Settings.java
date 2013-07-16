93f195b7fb1eefb14ab8df82aca60864eae20276
drcmda <drcmda@gmail.com>
2013-07-16 13:57:21 +0100

    HALO Size (1/2)
    Change-Id: I2780dfee66b0b80b57c62b83c40609104fe3c279

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3651,6 +3651,12 @@ public final class Settings {
         public static final String LED_BRIGHTNESS = "led_brightness";
 
         /**
+         * HALO size fraction, default is 1.0f (normal)
+         * @hide
+         */
+        public static final String HALO_SIZE = "halo_size";
+
+        /**
          * Whether to enable quiet hours.
          * @hide
          */
