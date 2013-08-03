dbb383912c7a93f300760a82fbbc2287f29b1463
travp624 <travp624@gmail.com>
2013-08-03 18:54:21 +0100

    Quicktiles: Add flip animation to Sync tile - v2. Copy-Paste exercise, all credit to TeamBAMF for the original work
    Change-Id: I96f2c8de26bb32a7d0964b26362c0740da2c14cf

--- packages/SystemUI/src/com/android/systemui/quicksettings/SyncTile.java
+++ packages/SystemUI/src/com/android/systemui/quicksettings/SyncTile.java
@@ -26,6 +26,9 @@ public class SyncTile extends QuickSettingsTile {
             public void onClick(View v) {
                 toggleState();
                 updateResources();
+                if (isFlipTilesEnabled()) {
+                    flipTile(0);
+                }
             }
         };
 
