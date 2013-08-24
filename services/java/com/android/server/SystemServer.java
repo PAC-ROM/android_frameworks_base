ec7e2ec33475b07cef4b6fa16119a6bf1e2623b1
Timo Wendt <timo@tjwendt.de>
2013-08-25 00:17:40 +0200

    Irda: Add Irda System Service for Samsung devices [working]
    needed for correct work
    http://review.cyanogenmod.org/46771 <- android_hardware_libhardware
    http://review.cyanogenmod.org/46769 <- android_device_samsung_jf-common
    http://review.cyanogenmod.org/47908 <- android_device_samsung_jf-common
    
    Change-Id: I5d9589251df6c12e0a6174e89c86dc9e13f7cd06

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -812,6 +812,16 @@ class ServerThread extends Thread {
             } catch (Throwable e) {
                 Slog.e(TAG, "Failure starting AssetRedirectionManager Service", e);
             }
+
+            if (context.getResources().getBoolean(
+                    com.android.internal.R.bool.config_enableIrdaManagerService)) {
+                try {
+                    Slog.i(TAG, "IrdaManager Service");
+                    ServiceManager.addService("irda", new IrdaManagerService(context));
+                } catch (Throwable e) {
+                    Slog.e(TAG, "Failure starting Irda Service", e);
+                }
+            }
         }
 
         // make sure the ADB_ENABLED setting value matches the secure property value
