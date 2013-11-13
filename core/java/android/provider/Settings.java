323edb68c690e242e71e07cda331987ac24d48e4
D4rKn3sSyS <jesdga95@gmail.com>
2013-11-13 18:50:32 +0100

    Lockscreen: See through (1/2)
    Add the ability to see through lockscreen.
    Adapted to kitkat from jellybean by @BigBrother1984
    
    Signed-off-by: D4rKn3sSyS <jesdga95@gmail.com>
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    
    PS2:
    
    * Adding missing sem-colon.
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    
    Change-Id: I9057a6e302a5a824799844f27e8f64ff677a520a
    
    PS3:
    
    * Replace and remove undefined mContext.
    * Define the boolean enableScreenRotation inside new method.
    * Whitespaces clean up.
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    
    PS4:
    
    * Restore mContext in setKeyguardParams().
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    
    PS5:
    
    * Fixes for setKeyguardParams() and maybeCreateKeyguardLocked() methods, kitkat changed some things.
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    
    PS6:
    
    * Missing sem-colon.
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    
    PS7: Fix all the things.
    
    * Effects doesn't vanish after first unlock.
    * Created condition in updateShowWallpaper() method that just return if see through is activated, he was restoring the wallpaper at second lock.
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    
    PS8: Cosmetic changes.
    
    * Correct java style name to seeThrough() and place it above updateShowWallpaper().
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    
    PS9: Other cosmetic changes.
    
    * Better name to the see through boolean.
    * Removed a space nitpick.
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    
    PS10:
    
    * We had two booleans for the same thing, ditch the one defined in one class only and take the global.
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    
    Change-Id: I06d952f85baf284961a24c8c9f90b29f4aef719a

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2771,13 +2771,6 @@ public final class Settings {
          */
         public static final String EGG_MODE = "egg_mode";
 
-       /**
-         * Animate-flip Quick Settings Panel Tiles on click
-         *
-         * @hide
-         */
-        public static final String QUICK_SETTINGS_TILES_FLIP = "quick_settings_tiles_flip";
-
         /**
          * Quick Settings Panel Tiles to Use
          *
@@ -3904,6 +3897,20 @@ public final class Settings {
         @Deprecated
         public static final String WIFI_WATCHDOG_PING_TIMEOUT_MS =
             Secure.WIFI_WATCHDOG_PING_TIMEOUT_MS;
+
+        /**
+         * Animate-flip Quick Settings Panel Tiles on click
+         *
+         * @hide
+         */
+        public static final String QUICK_SETTINGS_TILES_FLIP = "quick_settings_tiles_flip";
+
+        /**
+         * Allows to show the background activity back the lockscreen
+         * @hide
+         */
+        public static final String LOCKSCREEN_SEE_THROUGH = "lockscreen_see_through";
+
     }
 
     /**
--- packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
@@ -32,6 +32,7 @@ import android.app.ActivityManager;
 import android.app.WallpaperManager;
 import android.appwidget.AppWidgetManager;
 import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -39,6 +40,7 @@ import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.database.ContentObserver;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.ColorFilter;
@@ -48,12 +50,14 @@ import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.media.AudioManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
 import android.os.Parcelable;
 import android.os.PowerManager;
 import android.os.RemoteException;
 import android.os.SystemClock;
 import android.os.SystemProperties;
+import android.provider.Settings;
 import android.os.Vibrator;
 import android.provider.Settings;
 import android.util.Log;
@@ -115,6 +119,24 @@ public class KeyguardViewManager {
         void onShown(IBinder windowToken);
     };
 
+    class SettingsObserver extends ContentObserver {
+        SettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.LOCKSCREEN_SEE_THROUGH), false, this);
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            setKeyguardParams();
+            mViewManager.updateViewLayout(mKeyguardHost, mWindowLayoutParams);
+        }
+    }
+
     /**
      * @param context Used to create views.
      * @param viewManager Keyguard will be attached to this.
@@ -128,6 +150,9 @@ public class KeyguardViewManager {
         mViewManager = viewManager;
         mViewMediatorCallback = callback;
         mLockPatternUtils = lockPatternUtils;
+
+        SettingsObserver observer = new SettingsObserver(new Handler());
+        observer.observe();
     }
 
     /**
@@ -159,6 +184,41 @@ public class KeyguardViewManager {
         mKeyguardHost.setVisibility(View.VISIBLE);
         mKeyguardView.show();
         mKeyguardView.requestFocus();
+    }
+
+    public void setKeyguardParams() {
+        boolean enableScreenRotation = shouldEnableScreenRotation();
+
+        int flags = WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN
+                    | WindowManager.LayoutParams.FLAG_LAYOUT_INSET_DECOR
+                    | WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN;
+
+            if (!isSeeThroughEnabled()) {
+                flags |= WindowManager.LayoutParams.FLAG_SHOW_WALLPAPER;
+            }
+
+            if (!mNeedsInput) {
+                flags |= WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM;
+            }
+
+            final int stretch = ViewGroup.LayoutParams.MATCH_PARENT;
+            final int type = WindowManager.LayoutParams.TYPE_KEYGUARD;
+            WindowManager.LayoutParams lp = new WindowManager.LayoutParams(
+                    stretch, stretch, type, flags, PixelFormat.TRANSLUCENT);
+            lp.softInputMode = WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE;
+            lp.windowAnimations = R.style.Animation_LockScreen;
+            lp.screenOrientation = enableScreenRotation ?
+                    ActivityInfo.SCREEN_ORIENTATION_USER : ActivityInfo.SCREEN_ORIENTATION_NOSENSOR;
+
+            if (ActivityManager.isHighEndGfx()) {
+                lp.flags |= WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED;
+                lp.privateFlags |=
+                        WindowManager.LayoutParams.PRIVATE_FLAG_FORCE_HARDWARE_ACCELERATED;
+            }
+            lp.privateFlags |= WindowManager.LayoutParams.PRIVATE_FLAG_SET_NEEDS_MENU_KEY;
+            lp.inputFeatures |= WindowManager.LayoutParams.INPUT_FEATURE_DISABLE_USER_ACTIVITY;
+            lp.setTitle("Keyguard");
+            mWindowLayoutParams = lp;
     }
 
     private boolean shouldEnableScreenRotation() {
@@ -657,7 +717,14 @@ public class KeyguardViewManager {
         mViewManager.updateViewLayout(mKeyguardHost, mWindowLayoutParams);
     }
 
+    private boolean isSeeThroughEnabled() {
+        return Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.LOCKSCREEN_SEE_THROUGH, 0) == 1;
+    }
+
     void updateShowWallpaper(boolean show) {
+        if (isSeeThroughEnabled()) show = false;
+
         if (show) {
             mWindowLayoutParams.flags |= WindowManager.LayoutParams.FLAG_SHOW_WALLPAPER;
         } else {
