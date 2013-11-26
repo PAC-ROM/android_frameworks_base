cbd7f949460d1653d29f768c05d021f084ce3460
Dirk Rettschlag <dirk.rettschlag@gmail.com>
2013-11-26 19:54:44 +0100

    PAC Settings
    First try on adding a seperate PAC settings table.
    
    Patch Set 4: Fix Bootloop
    Patch Set 6: always try create to create the PAC table to
                 avoid changing the versioning and future merge issues
    Patch Set 7: Revert back to create the table in the latest version
                 handling code. This means when coming from an earlier
                 version it is handled correctly. Everyone else needs
                 to wipe.
    
    Change-Id: Ia058c31eab71be47f85e225e763218500b523ae6
    Signed-off-by: Dirk Rettschlag <dirk.rettschlag@gmail.com>

--- packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
+++ packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
@@ -93,6 +93,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String TABLE_SYSTEM = "system";
     private static final String TABLE_SECURE = "secure";
     private static final String TABLE_GLOBAL = "global";
+    private static final String TABLE_PAC = "pac";
 
     //Maximum number of phones
     private static final int MAX_PHONE_COUNT = 3;
@@ -101,6 +102,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         mValidTables.add(TABLE_SYSTEM);
         mValidTables.add(TABLE_SECURE);
         mValidTables.add(TABLE_GLOBAL);
+        mValidTables.add(TABLE_PAC);
         mValidTables.add("bluetooth_devices");
         mValidTables.add("bookmarks");
 
@@ -151,6 +153,15 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         db.execSQL("CREATE INDEX globalIndex1 ON global (name);");
     }
 
+    private void createPacTable(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE IF NOT EXISTS pac (" +
+                "_id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "name TEXT UNIQUE ON CONFLICT REPLACE," +
+                "value TEXT" +
+                ");");
+        db.execSQL("CREATE INDEX IF NOT EXISTS pacIndex1 ON pac (name);");
+    }
+
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("CREATE TABLE system (" +
@@ -161,6 +172,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         db.execSQL("CREATE INDEX systemIndex1 ON system (name);");
 
         createSecureTable(db);
+
+        createPacTable(db);
 
         // Only create the global table for the singleton 'owner' user
         if (mUserHandle == UserHandle.USER_OWNER) {
@@ -1576,6 +1589,14 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                     if (stmt != null) stmt.close();
                 }
             }
+            //add PAC table
+            db.beginTransaction();
+            try {
+                createPacTable(db);
+                db.setTransactionSuccessful();
+            } finally {
+                db.endTransaction();
+            }
             upgradeVersion = 98;
         }
 
@@ -1821,6 +1842,14 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                 db.endTransaction();
                 if (stmt != null) stmt.close();
             }
+            //add PAC table
+            db.beginTransaction();
+            try {
+                createPacTable(db);
+                db.setTransactionSuccessful();
+            } finally {
+                db.endTransaction();
+            }
             upgradeVersion = 113;
         }
 
@@ -2030,6 +2059,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             db.execSQL("DROP INDEX IF EXISTS bookmarksIndex1");
             db.execSQL("DROP INDEX IF EXISTS bookmarksIndex2");
             db.execSQL("DROP TABLE IF EXISTS favorites");
+            db.execSQL("DROP TABLE IF EXISTS pac");
+            db.execSQL("DROP INDEX IF EXISTS pacIndex1");
             onCreate(db);
 
             // Added for diagnosing settings.db wipes after the fact
@@ -2554,6 +2585,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         if (mUserHandle == UserHandle.USER_OWNER) {
             loadGlobalSettings(db);
         }
+        loadPacSettings(db);
     }
 
     private void loadSystemSettings(SQLiteDatabase db) {
@@ -2654,6 +2686,16 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         loadStringSetting(stmt, Settings.Secure.DEFAULT_THEME_PACKAGE, R.string.def_theme_package);
         loadStringSetting(stmt, Settings.Secure.DEFAULT_THEME_COMPONENTS,
                 R.string.def_theme_components);
+    }
+
+    private void loadPacSettings(SQLiteDatabase db) {
+        SQLiteStatement stmt = null;
+        try {
+            stmt = db.compileStatement("INSERT OR IGNORE INTO pac(name,value)"
+                    + " VALUES(?,?);");
+        } finally {
+            if (stmt != null) stmt.close();
+        }
     }
 
     private void loadSecureSettings(SQLiteDatabase db) {
--- packages/SettingsProvider/src/com/android/providers/settings/SettingsProvider.java
+++ packages/SettingsProvider/src/com/android/providers/settings/SettingsProvider.java
@@ -71,6 +71,7 @@ public class SettingsProvider extends ContentProvider {
     private static final String TABLE_GLOBAL = "global";
     private static final String TABLE_FAVORITES = "favorites";
     private static final String TABLE_OLD_FAVORITES = "old_favorites";
+    private static final String TABLE_PAC = "pac";
 
     private static final String[] COLUMN_VALUE = new String[] { "value" };
 
@@ -80,6 +81,8 @@ public class SettingsProvider extends ContentProvider {
     private static final SparseArray<SettingsCache> sSystemCaches
             = new SparseArray<SettingsCache>();
     private static final SparseArray<SettingsCache> sSecureCaches
+            = new SparseArray<SettingsCache>();
+   private static final SparseArray<SettingsCache> sPACCaches
             = new SparseArray<SettingsCache>();
     private static final SettingsCache sGlobalCache = new SettingsCache(TABLE_GLOBAL);
 
@@ -192,7 +195,7 @@ public class SettingsProvider extends ContentProvider {
                     throw new IllegalArgumentException("Bad root path: " + this.table);
                 }
                 if (TABLE_SYSTEM.equals(this.table) || TABLE_SECURE.equals(this.table) ||
-                    TABLE_GLOBAL.equals(this.table)) {
+                    TABLE_GLOBAL.equals(this.table) || TABLE_PAC.equals(this.table)) {
                     this.where = Settings.NameValueTable.NAME + "=?";
                     final String name = url.getPathSegments().get(1);
                     this.args = new String[] { name };
@@ -239,7 +242,8 @@ public class SettingsProvider extends ContentProvider {
         String table = tableUri.getPathSegments().get(0);
         if (TABLE_SYSTEM.equals(table) ||
                 TABLE_SECURE.equals(table) ||
-                TABLE_GLOBAL.equals(table)) {
+                TABLE_GLOBAL.equals(table) ||
+                TABLE_PAC.equals(table)) {
             String name = values.getAsString(Settings.NameValueTable.NAME);
             return Uri.withAppendedPath(tableUri, name);
         } else {
@@ -267,6 +271,9 @@ public class SettingsProvider extends ContentProvider {
             backedUpDataChanged = true;
         } else if (table.equals(TABLE_SECURE)) {
             property = Settings.Secure.SYS_PROP_SETTING_VERSION;
+            backedUpDataChanged = true;
+        } else if (table.equals(TABLE_PAC)) {
+            property = Settings.PAC.SYS_PROP_SETTING_VERSION;
             backedUpDataChanged = true;
         } else if (isGlobal) {
             property = Settings.Global.SYS_PROP_SETTING_VERSION;    // this one is global
@@ -411,6 +418,7 @@ public class SettingsProvider extends ContentProvider {
             mOpenHelpers.delete(userHandle);
             sSystemCaches.delete(userHandle);
             sSecureCaches.delete(userHandle);
+            sPACCaches.delete(userHandle);
             sKnownMutationsInFlight.delete(userHandle);
             onProfilesChanged();
         }
@@ -456,6 +464,7 @@ public class SettingsProvider extends ContentProvider {
 
                 sSystemCaches.append(userHandle, new SettingsCache(TABLE_SYSTEM));
                 sSecureCaches.append(userHandle, new SettingsCache(TABLE_SECURE));
+                sPACCaches.append(userHandle, new SettingsCache(TABLE_PAC));
                 sKnownMutationsInFlight.append(userHandle, new AtomicInteger(0));
             }
         }
@@ -517,6 +526,7 @@ public class SettingsProvider extends ContentProvider {
         }
         fullyPopulateCache(dbHelper, TABLE_SECURE, sSecureCaches.get(userHandle));
         fullyPopulateCache(dbHelper, TABLE_SYSTEM, sSystemCaches.get(userHandle));
+        fullyPopulateCache(dbHelper, TABLE_PAC, sPACCaches.get(userHandle));
     }
 
     // Slurp all values (if sane in number & size) into cache.
@@ -638,6 +648,9 @@ public class SettingsProvider extends ContentProvider {
         if (TABLE_SECURE.equals(tableName)) {
             return getOrConstructCache(callingUser, sSecureCaches);
         }
+        if (TABLE_PAC.equals(tableName)) {
+            return getOrConstructCache(callingUser, sPACCaches);
+        }
         if (TABLE_GLOBAL.equals(tableName)) {
             return sGlobalCache;
         }
@@ -734,6 +747,12 @@ public class SettingsProvider extends ContentProvider {
             return lookupValue(getOrEstablishDatabase(UserHandle.USER_OWNER), TABLE_GLOBAL,
                     sGlobalCache, request);
         }
+        if (Settings.CALL_METHOD_GET_PAC.equals(method)) {
+            if (LOCAL_LOGV) Slog.v(TAG, "call(pac:" + request + ") for " + callingUser);
+            dbHelper = getOrEstablishDatabase(callingUser);
+            cache = sPACCaches.get(callingUser);
+            return lookupValue(dbHelper, TABLE_PAC, cache, request);
+        }
 
         // Put methods - new value is in the args bundle under the key named by
         // the Settings.NameValueTable.VALUE static.
@@ -829,6 +848,9 @@ public class SettingsProvider extends ContentProvider {
                         + callingUser);
             }
             insertForUser(Settings.Global.CONTENT_URI, values, callingUser);
+        } else if (Settings.CALL_METHOD_PUT_PAC.equals(method)) {
+            if (LOCAL_LOGV) Slog.v(TAG, "call_put(pac:" + request + "=" + newValue + ") for " + callingUser);
+            insertForUser(Settings.PAC.CONTENT_URI, values, callingUser);
         } else {
             Slog.w(TAG, "call() with invalid method: " + method);
         }
