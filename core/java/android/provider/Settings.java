cd581d05240dc9fd77082fdb6717eb9996b0ebf5
maxwen <max.weninger@gmail.com>
2013-12-01 16:47:13 +0100

    [1/3] base: allow disabling non intrusive incall ui
    Change-Id: Ibe9c65f1fe024a859d98d96fd86199607a1060de

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3571,6 +3571,13 @@ public final class Settings {
          public static final String LISTVIEW_INTERPOLATOR = "listview_interpolator";
 
         /**
+         * Should the non-intrsive incall ui be used
+         *
+         * @hide
+         */
+        public static final String NON_INTRUSIVE_INCALL = "non_intrusive_incall";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
