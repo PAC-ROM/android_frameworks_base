4771aff02d3cee61ad6fa08d6adc123740459cd9
travp624 <travp624@gmail.com>
2013-12-02 00:03:17 -0600

    Frameworks: Lockscreen custom wallpaper (1/2)
    kitkat now has code for a custom wallpaper image.
    
    Using that code set a switch case to set the custom background between
      - Custom Wallpaper
      - Default background
    
    This way allows the same overlay shadow thats set on the keyguard when
    its the defualt way.
    
    Change-Id: I98fde63ef9f3c0a5f334cd9a97b237a6f99c57f6

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -55,6 +55,7 @@ import android.text.TextUtils;
 import android.util.AndroidException;
 import android.util.Log;
 
+import com.android.internal.util.cm.LockscreenBackgroundUtil;
 import com.android.internal.widget.ILockSettings;
 
 import java.net.URISyntaxException;
@@ -4427,6 +4428,14 @@ public final class Settings {
          * @hide
          */
         public static final String RECENTS_RAM_BAR_MODE = "recents_ram_bar_mode";
+
+        /**
+         * Sets the lockscreen background style. Integer.
+         * @see LockscreenBackgroundUtil#LOCKSCREEN_STYLE_DEFAULT
+         * @see LockscreenBackgroundUtil#LOCKSCREEN_STYLE_IMAGE
+         * @hide
+         */
+        public static final String LOCKSCREEN_BACKGROUND_STYLE = "lockscreen_background_style";
 
          /**
          * Action for long-pressing back button on lock screen
