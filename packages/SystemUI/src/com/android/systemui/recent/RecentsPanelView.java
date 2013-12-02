2025c72c5c8ee1e35b0a3f3738a944de3f6eb977
Daniel MM <daxx4182@gmail.com>
2013-12-03 00:30:21 +0800

    Recents: Google Recents
    Change-Id: I2207a0574a3c48aaf32b04f619be0f9242c350ab

--- packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
+++ packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
@@ -41,8 +41,10 @@ import android.graphics.Canvas;
 import android.graphics.Matrix;
 import android.graphics.Rect;
 import android.graphics.Shader.TileMode;
+import android.graphics.drawable.AnimationDrawable;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.graphics.PorterDuff.Mode;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.RemoteException;
@@ -110,6 +112,9 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
     private boolean mFitThumbnailToXY;
     private int mRecentItemLayoutId;
     private boolean mHighEndGfx;
+
+    private ImageView mRJingles;
+    private AnimationDrawable frameJingles;
 
     private ImageView mClearRecents;
     private LinearColorBar mRamUsageBar;
@@ -372,11 +377,14 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
             boolean noApps = mRecentTaskDescriptions != null
                     && (mRecentTaskDescriptions.size() == 0);
             mRecentsNoApps.setAlpha(1f);
+            mRJingles.setVisibility(noApps ? View.VISIBLE : View.INVISIBLE);
             mRecentsNoApps.setVisibility(noApps ? View.VISIBLE : View.INVISIBLE);
 	    mClearRecents.setVisibility(noApps ? View.GONE : View.VISIBLE);
 
             boolean showClearAllButton = Settings.System.getInt(mContext.getContentResolver(),
                     Settings.System.SHOW_CLEAR_RECENTS_BUTTON, 1) == 1;
+
+            mClearRecents.setColorFilter(getResources().getColor(R.color.status_bar_recents_app_label_color), Mode.SRC_ATOP);
             if (showClearAllButton) {
                 mClearRecents.setVisibility(noApps ? View.GONE : View.VISIBLE);
                 int clearAllButtonLocation = Settings.System.getInt(mContext.getContentResolver(),
@@ -508,6 +516,18 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
         mRecentsScrim = findViewById(R.id.recents_bg_protect);
         mRecentsNoApps = findViewById(R.id.recents_no_apps);
 	//mRecentsRamBar = findViewById(R.id.recents_ram_bar);
+
+        mRJingles = (ImageView) findViewById(R.id.recents_jingles);
+        mRJingles.setBackgroundResource(R.drawable.recents_jingles_animation);
+        frameJingles = (AnimationDrawable) mRJingles.getBackground();
+        if (mRJingles != null) {
+            mRJingles.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    frameJingles.start();
+                }
+            });
+        }
 
         mClearRecents = (ImageView) findViewById(R.id.recents_clear);
         if (mClearRecents != null){
