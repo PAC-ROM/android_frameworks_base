b4a968c7dd16d6334265210d273ec9af74f0cc8a
Kyrylo Mikos <kiril.mik.os@gmail.com>
2013-12-06 18:27:35 +0200

    SystemUI: unbreak RecentsPanel with translucent decors.
    Conflicts:
    	packages/SystemUI/res/layout-land/status_bar_recent_panel.xml
    	packages/SystemUI/res/layout/status_bar_recent_panel.xml
    	packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
    
    Change-Id: Ibcc4f5fc11109c0f2c149e66b7531e8c9ea79ae1

--- packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
+++ packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
@@ -39,6 +39,7 @@ import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Matrix;
+import android.graphics.Rect;
 import android.graphics.Shader.TileMode;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
@@ -1060,6 +1061,18 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
         } finally {
             reader.close();
         }
+    }
+
+    @Override
+    protected boolean fitSystemWindows(Rect insets) {
+        if (mClearRecents != null) {
+            MarginLayoutParams lp = (MarginLayoutParams) mClearRecents.getLayoutParams();
+            lp.topMargin = insets.top;
+            lp.rightMargin = insets.right;
+            mClearRecents.setLayoutParams(lp);
+        }
+
+        return super.fitSystemWindows(insets);
     }
 
     class FakeClearUserDataObserver extends IPackageDataObserver.Stub {
