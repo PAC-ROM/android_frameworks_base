b192beeae9a4377e2bde9bd900320a393b056541
cristianomatos <cristianobmatos@gmail.com>
2013-12-11 14:16:17 -0200

    Power menu into navring targets (1/2)
    - This commit use the intent added here: 0efd6981a6c4b299204b8d4f5495bddb5049f30e
    
    The intent added in  core/java/android/content/Intent.java is called when power menu is choosed
    
    Change-Id: Ifcf898ce7e3221fc67c18b2a54806ef344f68e7f

--- core/java/com/android/internal/util/cm/NavigationRingConstants.java
+++ core/java/com/android/internal/util/cm/NavigationRingConstants.java
@@ -26,6 +26,7 @@ public class NavigationRingConstants {
     public static final String ACTION_KILL_TASK =             "killTask";
     public static final String ACTION_MENU =                  "menu";
     public static final String ACTION_NONE =                  "none";
+    public static final String ACTION_POWER_MENU =            "powerMenu";
     public static final String ACTION_NOTIFICATIONS =         "notifications";
     public static final String ACTION_RECENTS =               "recents";
     public static final String ACTION_RING_SILENT_VIBRATE =   "ringVibrateSilent";
