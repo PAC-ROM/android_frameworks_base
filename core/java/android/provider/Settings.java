064d91e7db7d69aca85b8fa1f3745b9e519eefe5
cphelps76 <cphelps76@gmail.com>
2013-12-12 21:35:01 -0600

    Frameworks: Port BatteryBar (1/2)
    {x} Adds Battery bar with following features:
    {x} location set
    {x} color
    {x} aligment
    {x} thickness
    {x} charging animation
    
    Change-Id: Ie9a168121dc569ec3af068a4bda6b46a4e896b2d
    
    Frameworks: update battery bar default color to match kitkat ui (1/2)
    
    Change-Id: I4fc0b51abb29ac0a9f0b86e4d0ad3519f697b0f6

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4167,6 +4167,31 @@ public final class Settings {
          */
         public static final String RECENT_PANEL_SHOW_TOPMOST = "recent_panel_show_topmost";
 
+        /**
+         * Whether to show the battery bar
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR = "statusbar_battery_bar";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_COLOR = "statusbar_battery_bar_color";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_THICKNESS = "statusbar_battery_bar_thickness";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_STYLE = "statusbar_battery_bar_style";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_ANIMATE = "statusbar_battery_bar_animate";
     }
 
     /**
