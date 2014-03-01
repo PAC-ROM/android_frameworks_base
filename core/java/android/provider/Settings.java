c072400a7a8f18bb7a1b6b6058c5577f8517b82d
xplodwild <xplod@ouverta.fr>
2014-03-01 22:36:45 +0100

    [1/3] FW: Audio Themes Support
    From: https://gerrit.omnirom.org/#/c/5243/
    
    Change-Id: Ie4b1c40ad4f591b565969d4f1512dda30b670354

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2008,6 +2008,12 @@ public final class Settings {
         public static final String SCREEN_OFF_ANIMATION = "screen_off_animation";
 
         /**
+         * Defines the custom path to use for UI sound effects (null for default)
+         * @hide
+         */
+        public static final String CUSTOM_SOUND_EFFECTS_PATH = "custom_sound_effects_path";
+
+        /**
          * Control whether the process CPU usage meter should be shown.
          *
          * @deprecated Use {@link Global#SHOW_PROCESSES} instead
