50d3839498d7053a01b7e702bf79a9142c96d73b
Jubakuba <Jubakuba@gmail.com>
2014-03-07 20:20:28 +0100

    [1/2] Notification Reminder Interval && Fixes
    Original commit:
    
    Calculate actual length acceptable for the
    	flipper to flip through per device,
    	dpi, shade type, etc. A crespo user
    	emailed me and said they were having
    	issues in this area... Added benefit
    	is we can now have separate
    	flippers for landscape on devices
    	where landscape shade is longer than
    	portrait shade (non-tablets).
    	Create second flipper and set
    	visibility rather than doing the
    	complex string breaking on each rotation.
    
    Change EditText type to capitalize sentences
    	by default when setting message.
    
    Notification updated to allow user to stop
    	the sound by clicking and dismiss
    	with a button.  Allows a user to
    	cancel the sound but keep the
    	notification around.
    
    Also add Flip Interval option since (device dependant)
    	the strings parts can potentially be a
    	lot longer with this method.
    
    PS2: Oops forgot to git add and ammend lol
    PS3: Update file name so I don't conflict with
    	mnazim's upcoming feature lol
    PS4: Set title limit as well.  Use marquee
    	in the case that user's input is too
    	large to fit the view.
    PS6: Bad math is bad for layout padding
    
    --------------------------------------------------------
    
    PS2: update author
    PS3: update commit message
    PS4: miss one line in phonestatusbarview
    
    Change-Id: Id55475163e9b6d3472d33e6e326e6efc1076f3c8

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
@@ -176,6 +176,7 @@ public class PhoneStatusBarView extends PanelBar {
     public void onPanelPeeked() {
         super.onPanelPeeked();
         mBar.makeExpandedVisible();
+        mBar.setTakenSpace();
     }
 
     @Override
