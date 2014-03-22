d8aa361c442805fee3e9559cd313bec0c9e3f3e7
PrimeDirective <activethrasher00@gmail.com>
2014-03-22 12:44:28 +0100

    FW: Custom Scroll mod [1/2]
    ported from -> https://github.com/scotthartbti/android_frameworks_base/commit/423f7975c6b01b82e469600b836500b427d36418
    
    ported/modified from https://gerrit.omnirom.org/#/c/4251/
    
    originally written by Yanuar Harry <ai.the.smarties.physics@gmail.com>
    
    PS2: update author & message
    PS3: fix derp private --> public
    
    Change-Id: I0be00d138a4612c11154f35a8cfdfaca65880b51

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -8902,6 +8902,36 @@ public final class Settings {
          */
         public static final String TOAST_ANIMATION = "toast_animation";
 
+        /**
+         * Determine custom scroll friction.
+         * @hide
+         */
+        public static final String CUSTOM_SCROLL_FRICTION = "custom_scroll_friction";
+
+        /**
+         * Determine custom fling velocity.
+         * @hide
+         */
+        public static final String CUSTOM_FLING_VELOCITY = "custom_fling_velocity";
+
+        /**
+         * Determine custom overscroll distance.
+         * @hide
+         */
+        public static final String CUSTOM_OVERSCROLL_DISTANCE = "custom_overscroll_distance";
+
+        /**
+         * Determine custom overfling distance.
+         * @hide
+         */
+        public static final String CUSTOM_OVERFLING_DISTANCE = "custom_overfling_distance";
+
+        /**
+         *
+         * @hide
+         */
+        public static final String ANIMATION_CONTROLS_NO_SCROLL = "animation_controls_no_scroll";
+
     }
 
     /**
