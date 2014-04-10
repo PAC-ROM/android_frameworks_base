5c6cc64175e5d5a7383d2d3aed6df38d135cb599
Danesh Mondegarian <daneshm90@gmail.com>
2014-04-09 22:52:40 -0400

    Lockscreen: Fix SystemUI crash on initial selection of Custom Background.
    Change-Id: I8df4984f4b90d211baa888d380a95fc00a85d1ad

--- packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
@@ -119,15 +119,6 @@ public class KeyguardViewManager {
 
     private boolean mUnlockKeyDown = false;
 
-    private BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            if (Intent.ACTION_KEYGUARD_WALLPAPER_CHANGED.equals(intent.getAction())) {
-                mKeyguardHost.cacheUserImage();
-            }
-        }
-    };
-
     private KeyguardUpdateMonitorCallback mBackgroundChanger = new KeyguardUpdateMonitorCallback() {
         @Override
         public void onSetBackground(Bitmap bmp) {
@@ -194,10 +185,6 @@ public class KeyguardViewManager {
         mViewManager = viewManager;
         mViewMediatorCallback = callback;
         mLockPatternUtils = lockPatternUtils;
-
-        context.registerReceiver(mBroadcastReceiver,
-                new IntentFilter(Intent.ACTION_KEYGUARD_WALLPAPER_CHANGED),
-                android.Manifest.permission.CONTROL_KEYGUARD, null);
 
         SettingsObserver observer = new SettingsObserver(new Handler());
         observer.observe();
@@ -346,6 +333,14 @@ public class KeyguardViewManager {
             super(context);
             setBackground(mBackgroundDrawable);
             cacheUserImage();
+
+            context.registerReceiver(new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+                    cacheUserImage();
+                }
+            }, new IntentFilter(Intent.ACTION_KEYGUARD_WALLPAPER_CHANGED),
+                    android.Manifest.permission.CONTROL_KEYGUARD, null);
         }
 
         public void drawToCanvas(Canvas canvas, Drawable drawable) {
