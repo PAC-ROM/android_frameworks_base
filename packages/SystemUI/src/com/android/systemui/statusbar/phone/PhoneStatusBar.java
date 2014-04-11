aac3b775429e2511950606ffc7fc4f11f15709ba
Danny Baumann <dannybaumann@web.de>
2014-04-11 16:15:47 +0200

    Update settings after recreating status bar.
    This is needed to e.g. keep the circle battery after theme changes.
    
    Change-Id: Id1eb23d19c8cbc33c4987a908382f4b5fe17cfd4

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -3961,6 +3961,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             addNotificationViews(createNotificationViews(notifData.first, notifData.second));
         }
 
+        updateSettings();
         setAreThereNotifications();
 
         mStatusBarContainer.addView(mStatusBarWindow);
