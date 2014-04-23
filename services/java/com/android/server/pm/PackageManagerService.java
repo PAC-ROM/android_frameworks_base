ba64e492e886611217e8f3574517af3123cc11a4
Christopher R. Palmer <crpalmer@gmail.com>
2014-04-23 05:56:04 -0400

    PackageManager: Fix concurrent access to shared state
    Stop multiple threads from all calling add on the same HashMap.
    
    Cleanup the use of a boolean flag by removing concurrent writes
    to it.
    
    Change-Id: I8d61fe7294dcb67ee7d0d7de88d90d2051c310e6

--- services/java/com/android/server/pm/PackageManagerService.java
+++ services/java/com/android/server/pm/PackageManagerService.java
@@ -1279,7 +1279,7 @@ public class PackageManagerService extends IPackageManager.Stub {
                     Slog.w(TAG, "No BOOTCLASSPATH found!");
                 }
 
-                final boolean[] didDexOpt = {false};
+                boolean didDexOpt = false;
 
                 /**
                  * Ensure all external libraries have had dexopt run on them.
@@ -1294,12 +1294,13 @@ public class PackageManagerService extends IPackageManager.Stub {
                         }
                         try {
                             if (dalvik.system.DexFile.isDexOptNeeded(lib)) {
+                                alreadyDexOpted.add(lib);
+                                didDexOpt = true;
+
                                 executorService.submit(new Runnable() {
                                     @Override
                                     public void run() {
-                                        alreadyDexOpted.add(lib);
                                         mInstaller.dexopt(lib, Process.SYSTEM_UID, true);
-                                        didDexOpt[0] = true;
                                     }
                                 });
                             }
@@ -1350,11 +1351,12 @@ public class PackageManagerService extends IPackageManager.Stub {
                         }
                         try {
                             if (dalvik.system.DexFile.isDexOptNeeded(path)) {
+                                didDexOpt = true;
+
                                 executorService.submit(new Runnable() {
                                     @Override
                                     public void run() {
                                         mInstaller.dexopt(path, Process.SYSTEM_UID, true);
-                                        didDexOpt[0] = true;
                                     }
                                 });
                             }
@@ -1372,7 +1374,7 @@ public class PackageManagerService extends IPackageManager.Stub {
                     }
                 }
 
-                if (didDexOpt[0]) {
+                if (didDexOpt) {
                     File dalvikCacheDir = new File(dataDir, "dalvik-cache");
 
                     // If we had to do a dexopt of one of the previous
