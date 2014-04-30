fa284e5f85e53f14dca7dcde15e38c334c5e96f7
Veeti Paananen <veeti.paananen@rojekti.fi>
2014-04-30 07:52:41 +0300

    Temporarily hide lock screen unlock targets if opening a widget
    If the user selects a widget to open, it's very unlikely that they would
    want to use any of the slider shortcuts at that moment. Make it easier
    to unlock the screen and open the desired widget by temporarily hiding
    other targets.
    
    Change-Id: I51d879cc77826e4f7235c5d35b6352e5e83bb7cf

--- packages/Keyguard/src/com/android/keyguard/KeyguardSelectorView.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardSelectorView.java
@@ -468,6 +468,14 @@ public class KeyguardSelectorView extends LinearLayout implements KeyguardSecuri
             mGlowPadView.setDirectionDescriptionsResourceId(R.array.lockscreen_direction_descriptions);
         } else {
             mStoredTargets = storedTargets.split("\\|");
+
+            // Temporarily hide all targets if bouncing a widget
+            if (mIsBouncing) {
+                for (int i = 0; i < mStoredTargets.length; i++) {
+                    mStoredTargets[i] = LockscreenTargetUtils.EMPTY_TARGET;
+                }
+            }
+
             ArrayList<TargetDrawable> storedDrawables = new ArrayList<TargetDrawable>();
 
             final Resources res = getResources();
@@ -703,6 +711,7 @@ public class KeyguardSelectorView extends LinearLayout implements KeyguardSecuri
     @Override
     public void showBouncer(int duration) {
         mIsBouncing = true;
+        updateResources();
         KeyguardSecurityViewHelper.
                 showBouncer(mSecurityMessageDisplay, mFadeView, mBouncerFrame, duration);
     }
@@ -710,6 +719,7 @@ public class KeyguardSelectorView extends LinearLayout implements KeyguardSecuri
     @Override
     public void hideBouncer(int duration) {
         mIsBouncing = false;
+        updateResources();
         KeyguardSecurityViewHelper.
                 hideBouncer(mSecurityMessageDisplay, mFadeView, mBouncerFrame, duration);
     }
