fc55a7cd875bd830da73821dbc23e0780e60e797
Adnan <adnan@cyngn.com>
2014-06-20 16:51:49 -0700

    SystemUI: Expose heads up.
    Signed-off-by: Roman Birg <roman@cyngn.com>
    Signed-off-by: Adnan Begovic <adnan@cyngn.com>
    Change-Id: I276cb1c80d392b7dbac28e5f553207415b558481

--- packages/SystemUI/src/com/android/systemui/statusbar/tv/TvStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/tv/TvStatusBar.java
@@ -163,6 +163,10 @@ public class TvStatusBar extends BaseStatusBar {
     }
 
     @Override
+    public void hideHeadsUp() {
+    }
+
+    @Override
     public void animateExpandSettingsPanel(boolean flip) {
     }
 
--- services/java/com/android/server/InputMethodManagerService.java
+++ services/java/com/android/server/InputMethodManagerService.java
@@ -2040,6 +2040,11 @@ public class InputMethodManagerService extends IInputMethodManager.Stub
     }
 
     @Override
+    public boolean isImeShowing() {
+        return mInputShown;
+    }
+
+    @Override
     public void showInputMethodPickerFromClient(IInputMethodClient client) {
         if (!calledFromValidUser()) {
             return;
