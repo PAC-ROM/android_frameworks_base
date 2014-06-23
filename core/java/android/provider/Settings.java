ef9159a64f4d00d7581ed1ba1fe933f48c1ab737
cristianomatos <cristianobmatos@gmail.com>
2014-06-23 19:24:30 +0400

    Hover notification timeout [1/2]
    Allow user to set how much time the hover notification will stay visisible without touch the screen
    
    Change-Id: I4cd9ddb622b49fa9b553f6c259ce3b6a8407e13d

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4250,6 +4250,20 @@ public final class Settings {
         /** @hide */
         public static final String HOVER_FIRST_TIME = "hover_first_time";
 
+        /**
+         * Hover: Option to exclude topmost app, default is 1 (on).
+         *
+         * @hide
+         */
+        public static final String HOVER_EXCLUDE_TOPMOST = "hover_exclude_topmost";
+
+        /**
+         * Hover: long fade out delay, default is 5000ms (5s).
+         *
+         * @hide
+         */
+        public static final String HOVER_LONG_FADE_OUT_DELAY = "hover_long_fade_out_delay";
+
     }
 
     /**
