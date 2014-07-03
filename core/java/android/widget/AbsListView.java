8fa48d3e8012599983fb1e8dc829123de6d25462
Adnan Begovic <adnan@cyngn.com>
2014-07-03 04:02:00 +0000

    Revert "listview: ensure active views are filled prior than scrap views"
    This reverts commit 830a0024f1de9655d50c703ed1d10a81005db40e.
    
    See http://review.cyanogenmod.org/#/c/67348/
    
    Change-Id: I066e6ee8d9dc478c752ecfa161fd638bce7cee40

--- core/java/android/widget/AbsListView.java
+++ core/java/android/widget/AbsListView.java
@@ -6699,12 +6699,6 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
         private SparseArray<View> mTransientStateViews;
         private LongSparseArray<View> mTransientStateViewsById;
 
-        private boolean mIsActiveViewsInitialized;
-
-        boolean isActiveViewsInitialized() {
-            return mIsActiveViewsInitialized;
-        }
-
         public void setViewTypeCount(int viewTypeCount) {
             if (viewTypeCount < 1) {
                 throw new IllegalArgumentException("Can't have a viewTypeCount < 1");
@@ -6776,9 +6770,6 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
             }
 
             clearTransientStateViews();
-
-            // We want to a refresh of mActiveViews
-            mIsActiveViewsInitialized = false;
         }
 
         /**
@@ -6789,11 +6780,6 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
          *        mActiveViews
          */
         void fillActiveViews(int childCount, int firstActivePosition) {
-            // The recyclebin is initialized when we have some active views.
-            if (childCount > 0) {
-                mIsActiveViewsInitialized = true;
-            }
-
             if (mActiveViews.length < childCount) {
                 mActiveViews = new View[childCount];
             }
