thefutureosp/android_frameworks_base-1

Toggle Last App: move toggle last app method to a separate class in com/android/internal/util/crdroid/TaskUtils.java
- Use the modified class from OmniRom

Change-Id: Iec1aa13aa5fbbc53b4d166e6fe3b10b3f1ef9dca

packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -100,6 +100,7 @@ import com.android.systemui.statusbar.phone.PhoneStatusBar;
 import com.android.systemui.statusbar.phone.Ticker;
 import com.android.systemui.statusbar.policy.NotificationRowLayout;
 
+import com.android.internal.util.crdroid.TaskUtils;
 import com.android.internal.util.cm.DevUtils;
 
 import java.util.ArrayList;
@@ -914,35 +915,6 @@ public abstract class BaseStatusBar extends SystemUI implements
         }
     }
 
-    // Toggle last app on recents long press
-    protected void toggleLastAppImpl() {
-        int lastAppId = 0;
-        int looper = 1;
-        String packageName;
-        final Intent intent = new Intent(Intent.ACTION_MAIN);
-        final ActivityManager am = (ActivityManager) mContext
-                .getSystemService(Activity.ACTIVITY_SERVICE);
-        String defaultHomePackage = "com.android.launcher";
-        intent.addCategory(Intent.CATEGORY_HOME);
-        final ResolveInfo res = mContext.getPackageManager().resolveActivity(intent, 0);
-        if (res.activityInfo != null && !res.activityInfo.packageName.equals("android")) {
-            defaultHomePackage = res.activityInfo.packageName;
-        }
-        List <ActivityManager.RunningTaskInfo> tasks = am.getRunningTasks(5);
-        // lets get enough tasks to find something to switch to
-        // Note, we'll only get as many as the system currently has - up to 5
-        while ((lastAppId == 0) && (looper < tasks.size())) {
-            packageName = tasks.get(looper).topActivity.getPackageName();
-            if (!packageName.equals(defaultHomePackage) && !packageName.equals("com.android.systemui")) {
-                lastAppId = tasks.get(looper).id;
-            }
-            looper++;
-        }
-        if (lastAppId != 0) {
-            am.moveTaskToFront(lastAppId, am.MOVE_TASK_NO_USER_ACTION);
-        }
-    }
-
     protected void actionPowerMenu() {
         Intent intent = new Intent(Intent.ACTION_POWERMENU);
         mContext.sendBroadcast(intent);
@@ -1046,7 +1018,7 @@ public abstract class BaseStatusBar extends SystemUI implements
                   break;
              case MSG_TOGGLE_LAST_APP:
                  if (DEBUG) Log.d(TAG, "toggle last app");
-                 toggleLastAppImpl();
+                 TaskUtils.toggleLastAppImpl(mContext);
                  break;
              case MSG_TOGGLE_SCREENSHOT:
                  if (DEBUG) Slog.d(TAG, "toggle screenshot");

PAC-ROM/android_frameworks_base

packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -89,6 +89,7 @@ import com.android.systemui.statusbar.phone.KeyguardTouchDelegate;
 import com.android.systemui.statusbar.policy.NotificationRowLayout;
 
 import com.android.internal.util.cm.DevUtils;
+import com.android.internal.util.pac.TaskUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -779,35 +780,6 @@ public abstract class BaseStatusBar extends SystemUI implements
         }
     }
 
-    // Toggle last app on recents long press
-    protected void toggleLastAppImpl() {
-        int lastAppId = 0;
-        int looper = 1;
-        String packageName;
-        final Intent intent = new Intent(Intent.ACTION_MAIN);
-        final ActivityManager am = (ActivityManager) mContext
-                .getSystemService(Activity.ACTIVITY_SERVICE);
-        String defaultHomePackage = "com.android.launcher";
-        intent.addCategory(Intent.CATEGORY_HOME);
-        final ResolveInfo res = mContext.getPackageManager().resolveActivity(intent, 0);
-        if (res.activityInfo != null && !res.activityInfo.packageName.equals("android")) {
-            defaultHomePackage = res.activityInfo.packageName;
-        }
-        List <ActivityManager.RunningTaskInfo> tasks = am.getRunningTasks(5);
-        // lets get enough tasks to find something to switch to
-        // Note, we'll only get as many as the system currently has - up to 5
-        while ((lastAppId == 0) && (looper < tasks.size())) {
-            packageName = tasks.get(looper).topActivity.getPackageName();
-            if (!packageName.equals(defaultHomePackage) && !packageName.equals("com.android.systemui")) {
-                lastAppId = tasks.get(looper).id;
-            }
-            looper++;
-        }
-        if (lastAppId != 0) {
-            am.moveTaskToFront(lastAppId, am.MOVE_TASK_NO_USER_ACTION);
-        }
-    }
-
     protected void actionPowerMenu() {
         Intent intent = new Intent(Intent.ACTION_POWERMENU);
         mContext.sendBroadcast(intent);
@@ -923,7 +895,7 @@ public abstract class BaseStatusBar extends SystemUI implements
                   break;
              case MSG_TOGGLE_LAST_APP:
                  if (DEBUG) Log.d(TAG, "toggle last app");
-                 toggleLastAppImpl();
+                 TaskUtils.toggleLastAppImpl(mContext);
                  break;
              case MSG_TOGGLE_SCREENSHOT:
                  if (DEBUG) Slog.d(TAG, "toggle screenshot");