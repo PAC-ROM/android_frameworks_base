thefutureosp/android_frameworks_base-1

Navring last app toggle (1/2)
Change-Id: I5f744732f39cd33c0ec7dcae23da0295f945c9a1

packages/SystemUI/src/com/android/systemui/cm/ActionTarget.java
@@ -46,6 +46,7 @@ import android.view.KeyEvent;
 import android.widget.Toast;
 
 import com.android.internal.statusbar.IStatusBarService;
+import com.android.internal.util.crdroid.TaskUtils;
 import com.android.internal.util.cm.TorchConstants;
 import static com.android.internal.util.cm.NavigationRingConstants.*;
 import com.android.systemui.R;
@@ -105,6 +106,9 @@ public class ActionTarget {
 	} else if (action.equals(ACTION_POWER_MENU)) {
             Intent intent = new Intent(Intent.ACTION_POWERMENU);
 	    mContext.sendBroadcast(intent);
+            return true;
+        } else if (action.equals(ACTION_LAST_APP)) {
+            TaskUtils.toggleLastAppImpl(mContext);
             return true;
         } else if (action.equals(ACTION_IME)) {
             mContext.sendBroadcast(new Intent("android.settings.SHOW_INPUT_METHOD_PICKER"));

PAC-ROM/android_frameworks_base

packages/SystemUI/src/com/android/systemui/cm/ActionTarget.java
@@ -51,6 +51,7 @@ import android.widget.Toast;
 import com.android.internal.statusbar.IStatusBarService;
 import com.android.internal.util.cm.TorchConstants;
 import static com.android.internal.util.cm.NavigationRingConstants.*;
+import com.android.internal.util.pac.TaskUtils;
 import com.android.systemui.R;
 import com.android.systemui.screenshot.TakeScreenshotService;
 import com.android.systemui.statusbar.phone.KeyguardTouchDelegate;
@@ -113,6 +114,9 @@ public class ActionTarget {
         } else if (action.equals(ACTION_POWER_MENU)) {
             Intent intent = new Intent(Intent.ACTION_POWERMENU);
             mContext.sendBroadcast(intent);
+            return true;
+        } else if (action.equals(ACTION_LAST_APP)) {
+            TaskUtils.toggleLastAppImpl(mContext);
             return true;
         } else if (action.equals(ACTION_SCREENSHOT)) {
             takeScreenshot();