b1d0d7c9bb604de76f065e4202a308f09acbf82b
Owain van Brakel <owain.vanbrakel@gmail.com>
2014-08-07 22:09:49 +0200

    Halo tile
    Change-Id: I293bb2dc55ff65a5cd73059f263e26689a086f0f

---
+++ packages/SystemUI/src/com/android/systemui/quicksettings/HaloTile.java
@@ -0,0 +1,64 @@
+package com.android.systemui.quicksettings;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Handler;
+import android.provider.Settings;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnLongClickListener;
+
+import com.android.internal.view.RotationPolicy;
+import com.android.systemui.R;
+import com.android.systemui.statusbar.phone.QuickSettingsController;
+import com.android.systemui.statusbar.phone.QuickSettingsContainerView;
+
+public class HaloTile extends QuickSettingsTile {
+
+    public HaloTile(Context context, QuickSettingsController qsc, Handler handler) {
+        super(context, qsc);
+
+        mOnClick = new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Settings.PAC.putInt(mContext.getContentResolver(), Settings.PAC.HALO_ACTIVE, getHaloState() ? 1 : 0);
+            }
+        };
+
+        qsc.registerObservedContent(Settings.PAC.getUriFor(Settings.PAC.HALO_ACTIVE)
+                , this);
+    }
+
+    @Override
+    public void updateResources() {
+        updateTile();
+        updateQuickSettings();
+    }
+
+    private synchronized void updateTile() {
+        if(!getHaloState()){
+            mDrawable = R.drawable.ic_qs_halo_on;
+            mLabel = mContext.getString(R.string.halo_on);
+        }else{
+            mDrawable = R.drawable.ic_qs_halo_off;
+            mLabel = mContext.getString(R.string.halo_off);
+        }
+    }
+
+    @Override
+    void onPostCreate() {
+        updateTile();
+        super.onPostCreate();
+    }
+
+    private boolean getHaloState() {
+        return !Settings.PAC.getBoolean(mContext.getContentResolver(), Settings.PAC.HALO_ACTIVE, false);
+    }
+
+    @Override
+    public void onChangeUri(ContentResolver resolver, Uri uri) {
+        updateResources();
+    }
+}
