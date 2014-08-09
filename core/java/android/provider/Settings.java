8470cdaa84c8ecd152a4e8cfd77a53afa1b601ee
BjÃ¶rn Hutmacher <bjorn.hutmacher@gmail.com>
2014-08-09 22:19:38 +0200

    Revert "Frameworks: Policy: implement a pin keypad shuffler [1/2]"
    This reverts commit b462f1e98c26260e4e1e0b3983a96adf398fe251.
    
    Change-Id: If0e81d18cfb37b20b01daeeaedd07496117b9587

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -1035,7 +1035,6 @@ public final class Settings {
             MOVED_TO_SECURE.add(Secure.LOCK_PATTERN_ENABLED);
             MOVED_TO_SECURE.add(Secure.LOCK_PATTERN_VISIBLE);
             MOVED_TO_SECURE.add(Secure.LOCK_PATTERN_TACTILE_FEEDBACK_ENABLED);
-            MOVED_TO_SECURE.add(Secure.LOCK_NUMPAD_RANDOM);
             MOVED_TO_SECURE.add(Secure.LOCK_BEFORE_UNLOCK);
             MOVED_TO_SECURE.add(Secure.LOCK_PATTERN_SIZE);
             MOVED_TO_SECURE.add(Secure.LOCK_DOTS_VISIBLE);
@@ -4703,14 +4702,6 @@ public final class Settings {
          * Whether lock pattern is visible as user enters (0 = false, 1 = true)
          */
         public static final String LOCK_PATTERN_VISIBLE = "lock_pattern_visible_pattern";
-
-        /**
-         * Whether the NumKeyPad will change the orders of numbers
-         * in a PIN locked lockscreen
-         * 0 = off | 1 = always | 2 = only on request
-         * @hide
-         */
-        public static final String LOCK_NUMPAD_RANDOM = "lock_numpad_random";
 
         /**
          *Whether lock before unlock is enabled or disabled
