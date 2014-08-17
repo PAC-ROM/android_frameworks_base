9f49dc38e722898c3811fee96e4d1151e140ee3b
Owain van Brakel <owain.vanbrakel@gmail.com>
2014-08-17 22:01:37 +0200

    Add the title to the message since the title will be hidden on our boot dialog
    Change-Id: I77a8e250ce9960174105c67703e1471e9b8161f5

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -5924,7 +5924,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 if (DEBUG_BOOTMSG) Log.d(TAG, "********** showBootMessage(" + msg +", " + always + ") updated ***********");
                 if (currentPackageName != null) {
                     mBootMsgDialog.setTitle(msg);
-                    mBootMsgDialog.setMessage(currentPackageName);
+                    mBootMsgDialog.setMessage(msg + "\n" + currentPackageName);
                     if (DEBUG_BOOTMSG) Log.d(TAG, "setTitle: " + msg + " setMessage: " + currentPackageName);
                 } else {
                     if (DEBUG_BOOTMSG) Log.d(TAG, "failed; CURRENT_PACKAGE_NAME == null");
