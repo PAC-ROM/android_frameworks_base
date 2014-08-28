cd172e53b7c138ba4c3f9069f929eac8f2e47e1e
Danny Baumann <dannybaumann@web.de>
2014-08-28 10:49:36 +0200

    Clean up quick settings tile code.
    - Fix up coding style
    - Add copyright headers
    - Fix some multi-user bugs
    - Streamline code
    
    Change-Id: I0a118f7154bdd8cfdacdd7fdf8b76241010179de

--- packages/SystemUI/src/com/android/systemui/quicksettings/PreferencesTile.java
+++ packages/SystemUI/src/com/android/systemui/quicksettings/PreferencesTile.java
@@ -1,9 +1,25 @@
+/*
+ * Copyright (C) 2013-2014 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.android.systemui.quicksettings;
 
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.view.LayoutInflater;
+import android.provider.Settings;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnLongClickListener;
@@ -12,16 +28,14 @@ import com.android.systemui.R;
 import com.android.systemui.statusbar.phone.QuickSettingsContainerView;
 import com.android.systemui.statusbar.phone.QuickSettingsController;
 
-public class PreferencesTile extends QuickSettingsTile{
-
+public class PreferencesTile extends QuickSettingsTile {
     public PreferencesTile(Context context, QuickSettingsController qsc) {
         super(context, qsc);
 
         mOnClick = new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
-                startSettingsActivity(android.provider.Settings.ACTION_SETTINGS);
+                startSettingsActivity(Settings.ACTION_SETTINGS);
             }
         };
 
