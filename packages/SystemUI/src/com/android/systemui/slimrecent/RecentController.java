ezio84/frameworks_base

Fix recent panel defaulting to white on clean flash
Previously, recent panel would default to a solid
    white background on clean flash, until the user
    changed/reset the colors.  This fixes that issue,
    and the recent panel background now defaults to
    the proper semi-transparent white drawable (or
    semi-transparent black drawable if the TRDS
    state is switched).

Change-Id: Iea9d292596e8f4f51b55940e97e6597215ca58c2

packages/SystemUI/src/com/android/systemui/slimrecent/RecentController.java
@@ -101,7 +101,6 @@ public class RecentController implements RecentPanelView.OnExitListener,
     private int mLayoutDirection;
     private int mMainGravity;
     private int mUserGravity;
-    private Drawable defaultBackground;
     private int mPanelColor;
 
     private float mScaleFactor = DEFAULT_SCALE_FACTOR;
@@ -271,18 +270,17 @@ public class RecentController implements RecentPanelView.OnExitListener,
         if (mRecentPanelView != null) {
             mRecentPanelView.setMainGravity(mMainGravity);
         }
-        if (mMainGravity == Gravity.LEFT) {
-            defaultBackground = mContext.getResources().getDrawable(
-                R.drawable.recent_bg_dropshadow_left).getCurrent();
-        } else {
-            defaultBackground = mContext.getResources().getDrawable(
-                R.drawable.recent_bg_dropshadow).getCurrent();
-        }
+        
+        // Set custom background color (or reset to default, as the case may be
         if (mRecentContent != null) {
             if (mPanelColor != 0x00ffffff) {
                 mRecentContent.setBackgroundColor(mPanelColor);
             } else {
-                mRecentContent.setBackground(defaultBackground);
+                if (mMainGravity == Gravity.LEFT) {
+                    mRecentContent.setBackgroundResource(R.drawable.recent_bg_dropshadow_left);
+                } else {
+                    mRecentContent.setBackgroundResource(R.drawable.recent_bg_dropshadow);
+                }
             }
         }
     }
@@ -581,27 +579,20 @@ public class RecentController implements RecentPanelView.OnExitListener,
             }
 
             // Update colors in RecentPanelView
-            if (mMainGravity == Gravity.LEFT) {
-                defaultBackground = mContext.getResources().getDrawable(
-                    R.drawable.recent_bg_dropshadow_left).getCurrent();
-            } else {
-                defaultBackground = mContext.getResources().getDrawable(
-                    R.drawable.recent_bg_dropshadow).getCurrent();
-            }
-
             mPanelColor = Settings.System.getIntForUser(resolver,
-                    Settings.System.RECENT_PANEL_BG_COLOR, -2, UserHandle.USER_CURRENT);
+                    Settings.System.RECENT_PANEL_BG_COLOR, 0x00ffffff, UserHandle.USER_CURRENT);
 
             if (mPanelColor == Integer.MIN_VALUE
-                || mPanelColor == -2) {
-                // Flag to reset recent panel background color
-                mRecentContent.setBackground(defaultBackground);
+                || mPanelColor == -2
+                || mPanelColor == 0x00ffffff) {
+                    // Flag to reset recent panel background color
+                    if (mMainGravity == Gravity.LEFT) {
+                        mRecentContent.setBackgroundResource(R.drawable.recent_bg_dropshadow_left);
+                    } else {
+                        mRecentContent.setBackgroundResource(R.drawable.recent_bg_dropshadow);
+                    }
             } else {
-                if (mPanelColor != 0x00ffffff) {
                     mRecentContent.setBackgroundColor(mPanelColor);
-                } else {
-                    mRecentContent.setBackground(defaultBackground);
-                }
             }
         }
     }

PAC-ROM/android_frameworks_base

packages/SystemUI/src/com/android/systemui/slimrecent/RecentController.java
@@ -101,7 +101,6 @@ public class RecentController implements RecentPanelView.OnExitListener,
     private int mLayoutDirection;
     private int mMainGravity;
     private int mUserGravity;
-    private Drawable defaultBackground;
     private int mPanelColor;
 
     private float mScaleFactor = DEFAULT_SCALE_FACTOR;
@@ -271,18 +270,17 @@ public class RecentController implements RecentPanelView.OnExitListener,
         if (mRecentPanelView != null) {
             mRecentPanelView.setMainGravity(mMainGravity);
         }
-        if (mMainGravity == Gravity.LEFT) {
-            defaultBackground = mContext.getResources().getDrawable(
-                R.drawable.recent_bg_dropshadow_left).getCurrent();
-        } else {
-            defaultBackground = mContext.getResources().getDrawable(
-                R.drawable.recent_bg_dropshadow).getCurrent();
-        }
+        
+        // Set custom background color (or reset to default, as the case may be
         if (mRecentContent != null) {
             if (mPanelColor != 0x00ffffff) {
                 mRecentContent.setBackgroundColor(mPanelColor);
             } else {
-                mRecentContent.setBackground(defaultBackground);
+                if (mMainGravity == Gravity.LEFT) {
+                    mRecentContent.setBackgroundResource(R.drawable.recent_bg_dropshadow_left);
+                } else {
+                    mRecentContent.setBackgroundResource(R.drawable.recent_bg_dropshadow);
+                }
             }
         }
     }
@@ -581,27 +579,20 @@ public class RecentController implements RecentPanelView.OnExitListener,
             }
 
             // Update colors in RecentPanelView
-            if (mMainGravity == Gravity.LEFT) {
-                defaultBackground = mContext.getResources().getDrawable(
-                    R.drawable.recent_bg_dropshadow_left).getCurrent();
-            } else {
-                defaultBackground = mContext.getResources().getDrawable(
-                    R.drawable.recent_bg_dropshadow).getCurrent();
-            }
-
             mPanelColor = Settings.PAC.getIntForUser(resolver,
-                    Settings.PAC.RECENT_PANEL_BG_COLOR, -2, UserHandle.USER_CURRENT);
+                    Settings.PAC.RECENT_PANEL_BG_COLOR, 0x00ffffff, UserHandle.USER_CURRENT);
 
             if (mPanelColor == Integer.MIN_VALUE
-                || mPanelColor == -2) {
-                // Flag to reset recent panel background color
-                mRecentContent.setBackground(defaultBackground);
+                || mPanelColor == -2
+                || mPanelColor == 0x00ffffff) {
+                    // Flag to reset recent panel background color
+                    if (mMainGravity == Gravity.LEFT) {
+                        mRecentContent.setBackgroundResource(R.drawable.recent_bg_dropshadow_left);
+                    } else {
+                        mRecentContent.setBackgroundResource(R.drawable.recent_bg_dropshadow);
+                    }
             } else {
-                if (mPanelColor != 0x00ffffff) {
                     mRecentContent.setBackgroundColor(mPanelColor);
-                } else {
-                    mRecentContent.setBackground(defaultBackground);
-                }
             }
         }
     }