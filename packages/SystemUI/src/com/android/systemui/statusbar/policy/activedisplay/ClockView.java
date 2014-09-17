omnirom/android_frameworks_base_old

Base: fix ClockView ActiveDisplay
why no body report this bug?

this happen since AD merger into omnirom for the first time
i found this bug after using 24 time format LOL

Change-Id: I85be84914cc8210d9b0612994a4c25a5c6bfdeb6

packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ClockView.java
@@ -57,6 +57,7 @@ public class ClockView extends RelativeLayout {
     private TextView mTimeView;
     private TextView mDateView;
     private AmPm mAmPm;
+    private boolean showAmPm = false;
     private SettingsObserver mSettingsObserver;
     private ContentObserver mFormatChangeObserver;
     private int mAttached = 0; // for debugging - tells us whether attach/detach is unbalanced
@@ -184,12 +185,12 @@ public class ClockView extends RelativeLayout {
             ContentResolver resolver =
                     ClockView.this.mContext.getContentResolver();
 
-            boolean showAmPm = Settings.System.getInt(
+            showAmPm = Settings.System.getInt(
                     resolver, Settings.System.ACTIVE_DISPLAY_SHOW_AMPM, 0) == 1;
             boolean showDate = Settings.System.getInt(
                     resolver, Settings.System.ACTIVE_DISPLAY_SHOW_DATE, 0) == 1;
 
-            mAmPm.setShowAmPm(showAmPm);
+            setDateFormat();
             mDateView.setVisibility(showDate ? View.VISIBLE : View.INVISIBLE);
         }
     }
@@ -283,6 +284,6 @@ public class ClockView extends RelativeLayout {
 
     private void setDateFormat() {
         mFormat = android.text.format.DateFormat.is24HourFormat(getContext()) ? M24 : M12;
-        mAmPm.setShowAmPm(mFormat.equals(M12));
+        mAmPm.setShowAmPm(mFormat.equals(M12) && showAmPm);
     }
 }

PAC-ROM/android_frameworks_base

packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ClockView.java
@@ -57,6 +57,7 @@ public class ClockView extends RelativeLayout {
     private TextView mTimeView;
     private TextView mDateView;
     private AmPm mAmPm;
+    private boolean showAmPm = false;
     private SettingsObserver mSettingsObserver;
     private ContentObserver mFormatChangeObserver;
     private int mAttached = 0; // for debugging - tells us whether attach/detach is unbalanced
@@ -184,12 +185,12 @@ public class ClockView extends RelativeLayout {
             ContentResolver resolver =
                     ClockView.this.mContext.getContentResolver();
 
-            boolean showAmPm = Settings.PAC.getInt(
+            showAmPm = Settings.PAC.getInt(
                     resolver, Settings.PAC.ACTIVE_DISPLAY_SHOW_AMPM, 0) == 1;
             boolean showDate = Settings.PAC.getInt(
                     resolver, Settings.PAC.ACTIVE_DISPLAY_SHOW_DATE, 0) == 1;
 
-            mAmPm.setShowAmPm(showAmPm);
+            setDateFormat();
             mDateView.setVisibility(showDate ? View.VISIBLE : View.INVISIBLE);
         }
     }
@@ -283,6 +284,6 @@ public class ClockView extends RelativeLayout {
 
     private void setDateFormat() {
         mFormat = android.text.format.DateFormat.is24HourFormat(getContext()) ? M24 : M12;
-        mAmPm.setShowAmPm(mFormat.equals(M12));
+        mAmPm.setShowAmPm(mFormat.equals(M12) && showAmPm);
     }
 }