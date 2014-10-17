807e1c27edba1dcbc295e00e8a64486af7398126
Jubakuba <Jubakuba@gmail.com>
2014-10-17 00:39:40 -0600

    Framework: StatusBar Ticker (1/2)
    Change-Id: I6135297792c7b42172e08e14acd37b252cb00102

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -8109,6 +8109,12 @@ public final class Settings {
          */
         public static final String WAKELOCK_BLOCKING_LIST = "wakelock_blocking_list";
 
+        /**
+         * Whether to enable ticker animation in statusbar
+         * @hide
+         */
+        public static final String STATUS_BAR_TICKER_ENABLED = "status_bar_ticker_enabled";
+
     }
 
     /**
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -437,6 +437,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             //        Settings.System.STATUS_BAR_BATTERY_STYLE), false, this);
             //resolver.registerContentObserver(Settings.System.getUriFor(
             //        Settings.System.STATUS_BAR_SHOW_BATTERY_PERCENT), false, this);
+            resolver.registerContentObserver(Settings.PAC.getUriFor(
+                    Settings.PAC.STATUS_BAR_TICKER_ENABLED),
+                    false, this, UserHandle.USER_ALL);
             update();
         }
 
@@ -493,6 +496,12 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             mBatteryView.setShowPercent(showInsidePercent);
             //mBatteryTextView.setShowPercent(showNextPercent);
 */
+
+            mTickerEnabled = Settings.PAC.getIntForUser(
+                    resolver, Settings.PAC.STATUS_BAR_TICKER_ENABLED,
+                    mContext.getResources().getBoolean(R.bool.enable_ticker)
+                            ? 1 : 0, UserHandle.USER_CURRENT) == 1;
+
         }
     }
 
@@ -975,7 +984,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                         R.id.keyguard_indication_text));
         mKeyguardBottomArea.setKeyguardIndicationController(mKeyguardIndicationController);
 
-        mTickerEnabled = res.getBoolean(R.bool.enable_ticker);
+        mTickerEnabled = Settings.PAC.getIntForUser(mContext.getContentResolver(),
+                    Settings.PAC.STATUS_BAR_TICKER_ENABLED,
+                    mContext.getResources().getBoolean(R.bool.enable_ticker)
+                            ? 1 : 0, UserHandle.USER_CURRENT) == 1;
         if (mTickerEnabled) {
             final ViewStub tickerStub = (ViewStub) mStatusBarView.findViewById(R.id.ticker_stub);
             if (tickerStub != null) {
