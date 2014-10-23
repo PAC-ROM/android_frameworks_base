05fa207499c5474e8df2c05f8fd1fc91ef2039cd
cretin45 <cretin45@gmail.com>
2014-10-22 22:11:31 -0700

    SystemUI: Avoid NPEs during status bar recreation for MSIM
    PS2:
       * Clear the subs views on recreation
       * Unregister receivers on recreation
    
    Change-Id: Id131ffac497f46a753a97b479823e51fda7c412c

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -3878,6 +3878,13 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     private void recreateStatusBar() {
         mRecreating = true;
 
+        if (mMSimNetworkController != null) {
+            mMSimNetworkController.clearSubsLabelView();
+            mContext.unregisterReceiver(mMSimNetworkController);
+        } else if (mNetworkController != null) {
+            mContext.unregisterReceiver(mNetworkController);
+        }
+
         removeHeadsUpView();
 
         mStatusBarContainer.removeAllViews();
