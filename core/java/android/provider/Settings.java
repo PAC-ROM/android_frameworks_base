b18c6ebea662e1a5c47f99a3233ffbcae9f4defd
Adam Fisch <adammfisch@gmail.com>
2014-11-10 05:30:09 -0600

    Battery Bar [1/2]
    Change-Id: I6a12db17c61c7cc9a4b373a7cd8f810b5231309c

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -8336,6 +8336,32 @@ public final class Settings {
          */
         public static final String VOLUME_STEPS_VOICE_CALL = "volume_steps_voice_call";
 
+        /**
+         * Whether to show the battery bar
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR = "statusbar_battery_bar";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_COLOR = "statusbar_battery_bar_color";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_THICKNESS = "statusbar_battery_bar_thickness";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_STYLE = "statusbar_battery_bar_style";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_ANIMATE = "statusbar_battery_bar_animate";
+
     }
 
     /**
