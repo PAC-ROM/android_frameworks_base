51d2e5c91987f62ffb19ae18a3b6ddaff6e31d12
faust93 <monumentum@gmail.com>
2014-12-18 13:29:58 -0200

    Wakelock Blocker [1/2]
    Change-Id: I4197ef96e9f05f78cf72b69281ba3109a9e8f0f7

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -8777,6 +8777,18 @@ public final class Settings {
          */
         public static final String QS_SMART_PULLDOWN = "qs_smart_pulldown";
 
+        /**
+         * Enable blocking wakelock
+         * @hide
+         */
+        public static final String WAKELOCK_BLOCKING_ENABLED = "wakelock_blocking_enabled";
+
+        /**
+         * List of wakelock blocks selected
+         * @hide
+         */
+        public static final String WAKELOCK_BLOCKING_LIST = "wakelock_blocking_list";
+
     }
 
     /**
