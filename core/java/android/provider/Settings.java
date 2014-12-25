762fe8772774c77b35c0fbef14fd413904d2b623
Adnan Begovic <adnan@cyngn.com>
2014-12-25 05:25:11 +0000

    Headsup Options [1/2]
    - Global on/off switch
    - Blacklist and Do Not disturb
    
    Change-Id: I9b70ce7d2da1b46b42724c94d1c6c61fcffd74e2

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3533,6 +3533,27 @@ public final class Settings {
         public static final String LIVE_DISPLAY_HINTED = "live_display_hinted";
 
         /**
+         * Heads Up Notifications
+         *
+         * @hide
+         */
+        public static final String HEADS_UP_NOTIFICATION = "heads_up_enabled";
+
+        /**
+         * Which applications to disable heads up notifications for
+         *
+         * @hide
+         */
+        public static final String HEADS_UP_CUSTOM_VALUES = "heads_up_custom_values";
+
+        /**
+         * Which applications to disable heads up notifications for
+         *
+         * @hide
+         */
+        public static final String HEADS_UP_BLACKLIST_VALUES = "heads_up_blacklist_values";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
