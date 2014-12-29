1d7673bd329a2db483d49fadbaecc0e348447eb1
Stevespear426 <stephen.k.spear@gmail.com>
2014-12-29 16:03:42 -0200

    Base: All in One Animation Control [1/2]
    Allow configure System, ListView, Scroll, and Keyboard Animation
    
    * System from
      AOKP Animation Control https://github.com/AOKP/frameworks_base/commit/015c9cf00a8e49aea22d70b57b0af87c9134be73
      thanks to Steve Spear <stephen.k.spear@gmail.com>
    
    * ListView Animation https://gerrit.omnirom.org/#/c/2863/
      thanks to jkl5616<jkl5616@gmail.com>
    
    * Keyboard Animation https://github.com/zst123/XuiMod/commit/975cd531a9a847769b2af306495643a55ec44aaa
      thanks to zst123 for patches
    
    * with slight modification to OmniRom
    
    * Scrolling Animation https://github.com/zst123/XuiMod/commit/48c180cfa762617f59b4ceb3da50a6c28befe24f
      thanks to zst123 for patches
    
    * add toast animations
    
    * add separate option for them
    
    Change-Id: I89d880250867c41aecbb4cbe3ba1d473e780ebb4

--- core/java/android/inputmethodservice/InputMethodService.java
+++ core/java/android/inputmethodservice/InputMethodService.java
@@ -19,9 +19,11 @@ package android.inputmethodservice;
 import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
 import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
 
+import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.Dialog;
 import android.content.Context;
+import android.database.ContentObserver;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
@@ -44,6 +46,7 @@ import android.util.Log;
 import android.util.PrintWriterPrinter;
 import android.util.Printer;
 import android.view.Gravity;
+import android.view.IWindowManager;
 import android.view.KeyCharacterMap;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
@@ -54,7 +57,10 @@ import android.view.ViewTreeObserver;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.WindowManager.BadTokenException;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
 import android.view.animation.AnimationUtils;
+import android.view.animation.Interpolator;
 import android.view.inputmethod.CompletionInfo;
 import android.view.inputmethod.CursorAnchorInfo;
 import android.view.inputmethod.EditorInfo;
@@ -71,6 +77,7 @@ import android.widget.LinearLayout;
 
 import com.android.internal.statusbar.IStatusBarService;
 
+import com.android.internal.util.pac.AwesomeAnimationHelper;
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
 
@@ -317,6 +324,13 @@ public class InputMethodService extends AbstractInputMethodService {
     private IStatusBarService mStatusBarService;
     private Object mServiceAquireLock = new Object();
 
+    private Window mWindowIme;
+    private int mAnimationDuration;
+    private int mAnimationEnterIndex;
+    private int mAnimationExitIndex;
+    private int mInterpolatorIndex;
+
+    private SettingsObserver mSettingsObserver;
     final Insets mTmpInsets = new Insets();
     final int[] mTmpLocation = new int[2];
 
@@ -360,7 +374,47 @@ public class InputMethodService extends AbstractInputMethodService {
             }
         }
     };
-    
+
+    private class SettingsObserver extends ContentObserver {
+
+        SettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            getContentResolver().registerContentObserver(
+                    Settings.PAC.getUriFor(Settings.PAC.ANIMATION_IME_DURATION),
+                                    false, this);
+            getContentResolver().registerContentObserver(
+                    Settings.PAC.getUriFor(Settings.PAC.ANIMATION_IME_ENTER),
+                                    false, this);
+            getContentResolver().registerContentObserver(
+                    Settings.PAC.getUriFor(Settings.PAC.ANIMATION_IME_EXIT),
+                                    false, this);
+            getContentResolver().registerContentObserver(
+                    Settings.PAC.getUriFor(Settings.PAC.ANIMATION_IME_INTERPOLATOR),
+                                    false, this);
+
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            updateSettings();
+        }
+    }
+
+    private void updateSettings() {
+        mAnimationEnterIndex = Settings.PAC.getInt(getContentResolver(),
+                      Settings.PAC.ANIMATION_IME_ENTER, 0);
+        mAnimationExitIndex = Settings.PAC.getInt(getContentResolver(),
+                      Settings.PAC.ANIMATION_IME_EXIT, 0);
+        mInterpolatorIndex = Settings.PAC.getInt(getContentResolver(),
+                      Settings.PAC.ANIMATION_IME_INTERPOLATOR, 0);
+        int temp = Settings.PAC.getInt(getContentResolver(),
+                      Settings.PAC.ANIMATION_IME_DURATION, 0);
+        mAnimationDuration = temp * 15;
+    }
+
     /**
      * Concrete implementation of
      * {@link AbstractInputMethodService.AbstractInputMethodImpl} that provides
@@ -676,7 +730,8 @@ public class InputMethodService extends AbstractInputMethodService {
         return false;
     }
 
-    @Override public void onCreate() {
+    @Override
+    public void onCreate() {
         mTheme = Resources.selectSystemTheme(mTheme,
                 getApplicationInfo().targetSdkVersion,
                 android.R.style.Theme_InputMethod,
@@ -693,8 +748,13 @@ public class InputMethodService extends AbstractInputMethodService {
         if (mHardwareAccelerated) {
             mWindow.getWindow().addFlags(WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED);
         }
+        mHandler = new Handler();
+        mSettingsObserver = new SettingsObserver(mHandler);
+        mSettingsObserver.observe();
         initViews();
         mWindow.getWindow().setLayout(MATCH_PARENT, WRAP_CONTENT);
+        mWindowIme = mWindow.getWindow();
+        updateSettings();
     }
 
     /**
@@ -755,7 +815,8 @@ public class InputMethodService extends AbstractInputMethodService {
         mHandler = new Handler();
     }
 
-    @Override public void onDestroy() {
+    @Override
+    public void onDestroy() {
         super.onDestroy();
         mRootView.getViewTreeObserver().removeOnComputeInternalInsetsListener(
                 mInsetsComputer);
@@ -785,7 +846,8 @@ public class InputMethodService extends AbstractInputMethodService {
      * {@link #onCreateInputView} and {@link #onStartInputView} and related
      * appropriate functions if the UI is displayed.
      */
-    @Override public void onConfigurationChanged(Configuration newConfig) {
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
         
         boolean visible = mWindowVisible;
@@ -1598,7 +1660,17 @@ public class InputMethodService extends AbstractInputMethodService {
      * for the window has occurred (creating its views etc).
      */
     public void onWindowShown() {
-        // Intentionally empty
+        if (mAnimationEnterIndex == 0) {
+            mWindow.getWindow().setWindowAnimations(android.R.style.Animation_InputMethod);
+            return;
+        }
+        Dialog dialog = this.getWindow();
+        mWindowIme = dialog.getWindow();
+        mWindowIme.setWindowAnimations(-1);
+        dialog.show();
+        Animation anim = retrieveAnimation(true);
+        if (anim == null) return;
+        mRootView.startAnimation(anim);
     }
     
     /**
@@ -1606,7 +1678,56 @@ public class InputMethodService extends AbstractInputMethodService {
      * after previously being visible.
      */
     public void onWindowHidden() {
-        // Intentionally empty
+        if (mAnimationExitIndex == 0) {
+            mWindow.getWindow().setWindowAnimations(android.R.style.Animation_InputMethod);
+            return;
+        }
+        final Dialog dialog = this.getWindow();
+        mWindowIme = dialog.getWindow();
+        mWindowIme.setWindowAnimations(-1);
+
+        final Handler handler = new Handler();
+        final Runnable runnable = new Runnable() {
+                @Override
+                public void run() {
+                     dialog.hide();
+                }
+        };
+
+        Animation anim = retrieveAnimation(false);
+        if (anim != null){
+            anim.setAnimationListener(new AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {}
+                @Override
+                public void onAnimationRepeat(Animation animation) {}
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    handler.removeCallbacks(runnable);
+                    dialog.hide();
+                }
+            });
+            dialog.show();
+            mRootView.startAnimation(anim);
+            if (mAnimationDuration > 0) {
+                handler.postDelayed(runnable, (mAnimationDuration * 2));
+            } else {
+                handler.postDelayed(runnable, 1000);
+            }
+        }
+    }
+
+    private Animation retrieveAnimation(boolean enter){
+        int[] animArray = AwesomeAnimationHelper.getAnimations(enter ? mAnimationEnterIndex : mAnimationExitIndex);
+        int animInt = enter ? animArray[1] : animArray[0];
+        if (animInt == 0) return null;
+        Animation anim = AnimationUtils.loadAnimation(this, animInt);
+        Interpolator intplr= AwesomeAnimationHelper.getInterpolator(this, mInterpolatorIndex);
+        if (intplr != null) anim.setInterpolator(intplr);
+        if (mAnimationDuration > 0) {
+            anim.setDuration(mAnimationDuration);
+        }
+        return anim;
     }
     
     /**
@@ -2483,7 +2604,8 @@ public class InputMethodService extends AbstractInputMethodService {
      * Performs a dump of the InputMethodService's internal state.  Override
      * to add your own information to the dump.
      */
-    @Override protected void dump(FileDescriptor fd, PrintWriter fout, String[] args) {
+    @Override
+    protected void dump(FileDescriptor fd, PrintWriter fout, String[] args) {
         final Printer p = new PrintWriterPrinter(fout);
         p.println("Input method service state for " + this + ":");
         p.println("  mWindowCreated=" + mWindowCreated
