433cb74d0c34da0dfb2453b8cb20e717e0a0fa8b
Danesh M <daneshm90@gmail.com>
2015-01-27 21:17:42 -0800

    SystemUI : Navbar ring targets
    Change-Id: I113d9d257ffa0cb5a51823bee37bd86bb7f19527

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -1070,7 +1070,7 @@ public abstract class BaseStatusBar extends SystemUI implements
 
     @Override
     public void showSearchPanel() {
-        if (mSearchPanelView != null && mSearchPanelView.isAssistantAvailable()) {
+        if (mSearchPanelView != null) {
             mSearchPanelView.show(true, true);
         }
     }
