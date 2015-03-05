2107c75af8a1fb463ba59354527957097d1ddce6
Roman Birg <roman@cyngn.com>
2015-03-04 18:33:24 -0800

    SystemUI: improve qs tile scroll handling
      - place detail view within bounds of expanded panel when it
       goes beyond the bottom of the screen
    
      - collapse qs panel when scrolling detail view (and letting go),
        disallowing it to get stuck at weird states that cut off the detail view
    
    Change-Id: I775c0bb0022893a6a79fab93fb6cd92ba67b7213
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -23,9 +23,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.graphics.Point;
 import android.os.Handler;
 import android.os.Message;
-import android.os.UserHandle;
 import android.provider.Settings;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
@@ -70,6 +70,7 @@ public class QSPanel extends ViewGroup {
     private int mPanelPaddingBottom;
     private int mDualTileUnderlap;
     private int mBrightnessPaddingTop;
+    private int mTranslationTop;
     private boolean mExpanded;
     private boolean mListening;
 
@@ -82,6 +83,9 @@ public class QSPanel extends ViewGroup {
     private boolean mGridContentVisible = true;
 
     private boolean mUseMainTiles = false;
+
+    private DetailCallback mDetailCallback;
+    private int mContainerTop;
 
     public QSPanel(Context context) {
         this(context, null);
@@ -169,6 +173,14 @@ public class QSPanel extends ViewGroup {
 
     public void setCallback(Callback callback) {
         mCallback = callback;
+    }
+
+    public void setDetailCallback(DetailCallback callback) {
+        mDetailCallback = callback;
+    }
+
+    public void setTopOfContainer(int t) {
+        mContainerTop = t;
     }
 
     public void setHost(QSTileHost host) {
@@ -386,7 +398,7 @@ public class QSPanel extends ViewGroup {
         }
         int x = r.tileView.getLeft() + r.tileView.getWidth() / 2;
         int y = r.tileView.getTop() + r.tileView.getHeight() / 2;
-        handleShowDetailImpl(r, show, x, y);
+        handleShowDetailImpl(r, show, x, y - mTranslationTop);
     }
 
     private void handleShowDetailImpl(Record r, boolean show, int x, int y) {
@@ -470,9 +482,22 @@ public class QSPanel extends ViewGroup {
         if (mFooter.hasFooter()) {
             h += mFooter.getView().getMeasuredHeight();
         }
+
         mDetail.measure(exactly(width), MeasureSpec.UNSPECIFIED);
-        if (mDetail.getMeasuredHeight() < h) {
-            mDetail.measure(exactly(width), exactly(h));
+        if (isShowingDetail()) {
+            Point size = new Point();
+            getDisplay().getSize(size);
+            final int panelBottom = (mContainerTop - mTranslationTop + h);
+            final int detailMinHeight = size.y - mContainerTop - mPanelPaddingBottom;
+
+            if (size.y > panelBottom) {
+                int delta = size.y - panelBottom;
+                // panel is smaller than screen size
+                mDetail.measure(exactly(width), exactly(detailMinHeight - delta));
+            } else {
+                // panel is hanging below the screen
+                mDetail.measure(exactly(width), exactly(detailMinHeight));
+            }
         }
         setMeasuredDimension(width, Math.max(h, mDetail.getMeasuredHeight()));
     }
@@ -506,7 +531,7 @@ public class QSPanel extends ViewGroup {
             record.tileView.layout(left, top, right, top + record.tileView.getMeasuredHeight());
         }
         final int dh = Math.max(mDetail.getMeasuredHeight(), getMeasuredHeight());
-        mDetail.layout(0, 0, mDetail.getMeasuredWidth(), dh);
+        mDetail.layout(0, mTranslationTop, mDetail.getMeasuredWidth(), dh + mTranslationTop);
         if (mFooter.hasFooter()) {
             View footer = mFooter.getView();
             footer.layout(0, getMeasuredHeight() - footer.getMeasuredHeight(),
@@ -534,6 +559,10 @@ public class QSPanel extends ViewGroup {
         if (mCallback != null) {
             mCallback.onShowingDetail(detail);
         }
+        if (mDetailCallback != null && detail != null) {
+            // don't notify when hiding, wait for animation to finish
+            mDetailCallback.onDetailChanged(true);
+        }
     }
 
     private void fireToggleStateChanged(boolean state) {
@@ -554,6 +583,12 @@ public class QSPanel extends ViewGroup {
         final boolean scanState = mDetailRecord instanceof TileRecord
                 && ((TileRecord) mDetailRecord).scanState;
         fireScanStateChanged(scanState);
+    }
+
+    public void setDetailOffset(int translationY) {
+        if (!isShowingDetail()) {
+            mTranslationTop = translationY;
+        }
     }
 
     private class H extends Handler {
@@ -586,6 +621,9 @@ public class QSPanel extends ViewGroup {
         public void onAnimationEnd(Animator animation) {
             mDetailContent.removeAllViews();
             setDetailRecord(null);
+            if (mDetailCallback != null) {
+                mDetailCallback.onDetailChanged(false);
+            }
         };
     };
 
@@ -601,6 +639,10 @@ public class QSPanel extends ViewGroup {
             setGridContentVisibility(false);
         }
     };
+
+    public interface DetailCallback {
+        void onDetailChanged(boolean showing);
+    }
 
     public interface Callback {
         void onShowingDetail(QSTile.DetailAdapter detail);
