b27bad599c502321b9143d0e151fbf4beb6e12be
Jorge Ruesga <jorge@ruesga.com>
2015-06-11 02:39:36 +0200

    base: dynamic tiles
    Add support for dynamic tiles
    
    Video: https://cloud.ruesga.com/f/71a12e4801/
    Required: topic:customtiles_respkg
              topic:readd_customtiles
    JIRA: CML-22
    
    Change-Id: Ie17ebf09ee88bc0c53561decc27430e174814543
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -389,6 +389,7 @@ public class NotificationPanelView extends PanelView implements
             @Override
             public void onAnimationEnd(Animator animation) {
                 mQsSizeChangeAnimator = null;
+                mQsContainer.setHeightOverride(-1);
             }
         });
         mQsSizeChangeAnimator.start();
