ca379cfdebd11852bca55393d91b6ae8eb9132fd
Matt Garnes <matt@cyngn.com>
2015-06-29 15:04:41 -0700

    Wait until the Keyguard is drawn to dismiss the boot dialog.
    In a631d49, we check that the keyguard is ready to avoid displaying the
    homescreen before the keyguard if the boot message is dismissed too
    soon. However, this only checked mKeyguardDrawnOnce, which is set to
    true before the keyguard is fully visible to the user. Instead, wait for
    mKeyguardDrawComplete, which will be triggered when the Keyguard is
    actually fully visible.
    
    With this change, the keyguard should always be the first thing displayed
    after the boot dialog is dismissed, not the home screen.
    
    Change-Id: I92f7df66e04613e9f9b39d293b190fc2473de176

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -352,7 +352,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     int[] mNavigationBarHeightForRotation = new int[4];
     int[] mNavigationBarWidthForRotation = new int[4];
 
-    boolean mBootMessageNeedsHiding;
     KeyguardServiceDelegate mKeyguardDelegate;
     final Runnable mWindowManagerDrawCallback = new Runnable() {
         @Override
@@ -366,6 +365,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         public void onShown(IBinder windowToken) {
             if (DEBUG_WAKEUP) Slog.d(TAG, "mKeyguardDelegate.ShowListener.onShown.");
             mHandler.sendEmptyMessage(MSG_KEYGUARD_DRAWN_COMPLETE);
+            hideBootMessages();
         }
     };
 
@@ -6192,10 +6192,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             if (!mKeyguardDrawnOnce && mAwake) {
                 mKeyguardDrawnOnce = true;
                 enableScreen = true;
-                if (mBootMessageNeedsHiding) {
-                    mBootMessageNeedsHiding = false;
-                    hideBootMessages();
-                }
             } else {
                 enableScreen = false;
             }
@@ -6215,9 +6211,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     private void handleHideBootMessage() {
         synchronized (mLock) {
-            if (!mKeyguardDrawnOnce) {
-                mBootMessageNeedsHiding = true;
-                return; // keyguard hasn't drawn the first time yet, not done booting
+            if (!mKeyguardDrawComplete) {
+                return; // keyguard hasn't completed drawing, not done booting.
             }
         }
 
