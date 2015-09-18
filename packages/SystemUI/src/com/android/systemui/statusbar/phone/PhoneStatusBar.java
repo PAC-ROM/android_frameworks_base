4c9cc13d2a0c191719dc2c5f75ccf4bcffdba6e8
Alexander Martinz <eviscerationls@gmail.com>
2015-09-18 23:11:33 +0200

    SystemUI: statusbar: cache and reuse power manager
    Change-Id: If3a67b4485e083f9a3d2b71f1db4eadfe127c80b
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -327,6 +327,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     private boolean mScreenOnComingFromTouch;
     private PointF mScreenOnTouchLocation;
 
+    private PowerManager mPowerManager;
+
     int mPixelFormat;
     Object mQueueLock = new Object();
 
@@ -1008,6 +1010,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
     @Override
     public void start() {
+        mPowerManager = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
+
         mDisplay = ((WindowManager)mContext.getSystemService(Context.WINDOW_SERVICE))
                 .getDefaultDisplay();
         updateDisplaySize();
@@ -1021,7 +1025,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             mCurrentTheme = ThemeConfig.getSystemTheme();
         }
 
-        mStatusBarWindow = new StatusBarWindowView(mContext, null);
+        mStatusBarWindow = new StatusBarWindowView(mContext, null, mPowerManager);
         mStatusBarWindow.mService = this;
 
         super.start(); // calls createAndAddWindows()
@@ -5555,8 +5559,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
     public void wakeUpIfDozing(long time, MotionEvent event) {
         if (mDozing && mDozeScrimController.isPulsing()) {
-            PowerManager pm = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
-            pm.wakeUp(time);
+            mPowerManager.wakeUp(time);
             mScreenOnComingFromTouch = true;
             mScreenOnTouchLocation = new PointF(event.getX(), event.getY());
             mNotificationPanel.setTouchDisabled(false);
