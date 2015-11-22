9f551f59efe483d3a19cc78caf84d1632bdb7b12
cphelps76 <cphelps76@droidconcepts.com>
2015-11-22 13:53:07 +0100

    BatteryBar [1/2]
    {x} Adds Battery bar with following features:
    {x} location set
    {x} color
    {x} aligment
    {x} thickness
    {x} charging animation
    
    Change-Id: I121bdc5c5993ff65ab6fbd4f387ca1bc26ffb6fc

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3536,6 +3536,32 @@ public final class Settings {
         public static final String VOLUME_KEYS_CONTROL_RING_STREAM =
                 "volume_keys_control_ring_stream";
 
+       /**
+        * Whether to show the battery bar
+        * @hide
+        */
+        public static final String STATUSBAR_BATTERY_BAR = "statusbar_battery_bar";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_COLOR = "statusbar_battery_bar_color";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_THICKNESS = "statusbar_battery_bar_thickness";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_STYLE = "statusbar_battery_bar_style";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_ANIMATE = "statusbar_battery_bar_animate";
+
         /**
          * Whether to use the custom status bar header or not
          * @hide
