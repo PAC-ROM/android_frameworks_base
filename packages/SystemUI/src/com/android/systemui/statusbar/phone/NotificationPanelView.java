f6fda725340b53ed3b0c72185468e0cf5fb192de
d34d <clark@cyngn.com>
2016-05-16 15:23:14 -0700

    Lls: Use LlsController for checking LLS focus
    We're trying to keep track of this state in too many places.  The
    LiveLockScreenController keeps track of the LLS focus so this patch
    adds a method to get the state of the LLS focus and use that instead
    of cached values that may not be in sync.
    
    Change-Id: I2cbf1122b8a27cb1b0ee1ed93379a362308db5bd
    TICKET: CYNGNOS-2849

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -261,7 +261,6 @@ public class NotificationPanelView extends PanelView implements
     private SwipeLockedDirection mLockedDirection;
 
     private SwipeHelper mSwipeHelper;
-    public boolean mShowingExternalKeyguard;
     private final int mMinimumFlingVelocity;
     private final int mScreenHeight;
     private LiveLockScreenController mLiveLockscreenController;
@@ -295,7 +294,6 @@ public class NotificationPanelView extends PanelView implements
 
         @Override
         public void onChildDismissed(View v) {
-            mShowingExternalKeyguard = true;
             mCanDismissKeyguard = false;
             mStatusBar.focusKeyguardExternalView();
             mLiveLockscreenController.onLiveLockScreenFocusChanged(true /* hasFocus */);
@@ -362,7 +360,6 @@ public class NotificationPanelView extends PanelView implements
                     return false;
                 }
                 mCanDismissKeyguard = true;
-                mShowingExternalKeyguard = false;
                 mStatusBar.showBouncer();
                 return true;
             }
@@ -451,7 +448,7 @@ public class NotificationPanelView extends PanelView implements
             @Override
             public boolean onInterceptTouchEvent(MotionEvent e) {
                 boolean intercept = false;
-                if (mShowingExternalKeyguard) {
+                if (mLiveLockscreenController.getLiveLockScreenHasFocus()) {
                     // Handles swipe up to fade/dismiss when showing
                     // live lock screen
                     intercept = mAfforanceHelper.onInterceptTouchEvent(e);
@@ -476,7 +473,7 @@ public class NotificationPanelView extends PanelView implements
                 }
 
                 boolean intercept = false;
-                if (mShowingExternalKeyguard) {
+                if (mLiveLockscreenController.getLiveLockScreenHasFocus()) {
                     intercept = mAfforanceHelper.onTouchEvent(e);
                     if (isCancelOrUp) {
                         mKeyguardBottomArea.expand(false);
@@ -1010,7 +1007,8 @@ public class NotificationPanelView extends PanelView implements
         }
         if ((!mIsExpanding || mHintAnimationRunning)
                 && !mQsExpanded
-                && (mStatusBar.getBarState() != StatusBarState.SHADE || mShowingExternalKeyguard)) {
+                && (mStatusBar.getBarState() != StatusBarState.SHADE
+                || mLiveLockscreenController.getLiveLockScreenHasFocus())) {
             mAfforanceHelper.onTouchEvent(event);
         }
         if (mOnlyAffordanceInThisMotion) {
@@ -1052,7 +1050,8 @@ public class NotificationPanelView extends PanelView implements
     }
 
     private boolean isKeyguardInteractiveAndShowing() {
-        return mShowingExternalKeyguard || mStatusBar.getBarState() != StatusBarState.KEYGUARD ||
+        return mLiveLockscreenController.getLiveLockScreenHasFocus() ||
+                mStatusBar.getBarState() != StatusBarState.KEYGUARD ||
                 !mLiveLockscreenController.isLiveLockScreenInteractive();
     }
 
@@ -1357,9 +1356,6 @@ public class NotificationPanelView extends PanelView implements
         if (statusBarState == StatusBarState.KEYGUARD ||
                 statusBarState == StatusBarState.SHADE_LOCKED) {
             updateDozingVisibilities(false /* animate */);
-        }
-        if (statusBarState == StatusBarState.KEYGUARD) {
-            mShowingExternalKeyguard = false;
         }
         resetVerticalPanelPosition();
         updateQsState();
@@ -2090,7 +2086,7 @@ public class NotificationPanelView extends PanelView implements
 
     private void updateKeyguardBottomAreaAlpha() {
         float alpha = Math.min(getKeyguardContentsAlpha(), 1 - getQsExpansionFraction());
-        if (mShowingExternalKeyguard) {
+        if (mLiveLockscreenController.getLiveLockScreenHasFocus()) {
             alpha = 1f;
         }
         mKeyguardBottomArea.setAlpha(alpha);
